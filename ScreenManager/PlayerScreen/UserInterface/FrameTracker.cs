6556d8506a1a55c0d123a5fac3e5f790e33e1c2b
Joan Charmant <jcharmant@gmail.com>
2010-01-16 14:08:53 +0000

    Added markers in the frame tracker gutter to see stopwatches and paths. Changed the appearance of the markers a bit.

--- ScreenManager/PlayerScreen/UserInterface/FrameTracker.cs
+++ ScreenManager/PlayerScreen/UserInterface/FrameTracker.cs
@@ -20,6 +20,7 @@ along with Kinovea. If not, see http://www.gnu.org/licenses/.
 
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Windows.Forms;
@@ -28,7 +29,7 @@ namespace Kinovea.ScreenManager
 {
 	/// <summary>
 	/// A control to let the user specify the current position in the video.
-	/// The control is comprised of a cursor and a list of hairlines for keyframes
+	/// The control is comprised of a cursor and a list of markers.
 	/// 
 	/// When control is modified by user:
 	/// - The internal data is modified.
@@ -38,7 +39,7 @@ namespace Kinovea.ScreenManager
 	/// When control appearence needs to be updated
 	/// - This is when internal data of the parent control have been modified by other means.
 	/// - (At initialization for example)
-	/// - The public properties setters are provided, they doesn't raise the events back. 
+	/// - The public properties setters are provided, they doesn't raise the events back.
 	/// </summary>
     public partial class FrameTracker : UserControl
     {
@@ -91,14 +92,29 @@ namespace Kinovea.ScreenManager
         private long m_iMaximum = 0;
         private int m_iMaxWidth = 0;
         private bool m_bReportOnMouseMove = false;
-        private long[] m_KeyframesTimestamps;
-        private int[] m_KeyframesMarks;
+        private bool m_bEnabled = true;
+       	
+        // Markers
+        private Metadata m_Metadata;
+        
+        private List<int> m_KeyframesMarks = new List<int>();			// In control coordinates.
+        private static readonly Pen m_PenKeyBorder = new Pen(Color.FromArgb(255, Color.YellowGreen), 1);
+        private static readonly Pen m_PenKeyInside = new Pen(Color.FromArgb(96, Color.YellowGreen), 1);
+        
+        private List<Point> m_ChronosMarks = new List<Point>();			// Start and end of chronos in control coords.
+        private static readonly Pen m_PenChronoBorder = new Pen(Color.FromArgb(255, Color.CornflowerBlue), 1); // skyblue
+        private static readonly SolidBrush m_BrushChrono = new SolidBrush(Color.FromArgb(96, Color.CornflowerBlue));
+        
+        private List<Point> m_TracksMarks = new List<Point>();			// Start and end of tracks in control coords.
+        private static readonly Pen m_PenTrackBorder = new Pen(Color.FromArgb(255, Color.Plum), 1); // Plum;SandyBrown
+        private static readonly SolidBrush m_BrushTrack = new SolidBrush(Color.FromArgb(96, Color.Plum));
+        
         private long m_SyncPointTimestamp;
         private int m_SyncPointMark;
-        private bool m_bEnabled = true;
-       	private static readonly Pen m_PenKeyImageMark = new Pen(Color.FromArgb(192, Color.YellowGreen), 2); // YellowGreen
-        private static readonly Pen m_PenSyncPointMark = new Pen(Color.FromArgb(192, Color.Firebrick), 2); // 
-       	private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private static readonly Pen m_PenSyncBorder = new Pen(Color.FromArgb(255, Color.Firebrick), 1);
+        private static readonly Pen m_PenSyncInside = new Pen(Color.FromArgb(96, Color.Firebrick), 1);
+       	
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         #endregion
 
         #region Events Delegates
@@ -120,24 +136,6 @@ namespace Kinovea.ScreenManager
 		#endregion
 		
 		#region Public Methods
-		public void UpdateKeyframesMarkers(long[] _markers)
-		{
-			if (m_KeyframesTimestamps == null)
-            {
-                m_KeyframesTimestamps = new long[_markers.Length];
-            }
-            else if (m_KeyframesTimestamps.Length != _markers.Length)
-            {
-                m_KeyframesTimestamps = new long[_markers.Length];
-            }
-
-            for (int i = 0; i < _markers.Length; i++)
-            {
-                m_KeyframesTimestamps[i] = _markers[i];
-            }
-
-            UpdateAppearence();
-		}
 		public void Remap(long _iMin, long _iMax)
         {
         	// This method is only a shortcut to updating min and max properties at once.
@@ -150,15 +148,23 @@ namespace Kinovea.ScreenManager
         	
         	UpdateAppearence();
         }
-		public void UpdateSyncPointMarker(long _marker)
-		{
-			m_SyncPointTimestamp = _marker;
-			UpdateAppearence();
-		}
 		public void EnableDisable(bool _bEnable)
 		{
 			m_bEnabled = _bEnable;
 			NavCursor.Enabled = _bEnable;
+		}
+		public void UpdateMarkers(Metadata _metadata)
+		{
+			// Keep a ref on the Metadata object so we can update the
+			// markers position when only the size of the control changes.
+			
+			m_Metadata = _metadata;
+			UpdateAppearence();
+		}
+		public void UpdateSyncPointMarker(long _marker)
+		{
+			m_SyncPointTimestamp = _marker;
+			UpdateAppearence();
 		}
 		#endregion
         
@@ -220,21 +226,66 @@ namespace Kinovea.ScreenManager
         }
         private void FrameTracker_Paint(object sender, PaintEventArgs e)
         {
-            if (m_KeyframesMarks != null)
+        	// Draw the various markers within the frame tracker gutter.
+        	if (m_KeyframesMarks.Count > 0)
             {
                 foreach (int mark in m_KeyframesMarks)
                 {
                     if (mark > 0)
                     {
-                        e.Graphics.DrawLine(m_PenKeyImageMark, mark, 2, mark, this.Height - 4);
+                    	DrawMark(e.Graphics, m_PenKeyBorder, m_PenKeyInside, mark);
                     }
                 }
             }
+        	
+        	if(m_ChronosMarks.Count > 0)
+        	{
+        		foreach (Point mark in m_ChronosMarks)
+                {
+                	DrawMark(e.Graphics, m_PenChronoBorder, m_BrushChrono, mark);
+                }
+        	}
+        	
+        	if(m_TracksMarks.Count > 0)
+        	{
+        		foreach (Point mark in m_TracksMarks)
+                {
+                	DrawMark(e.Graphics, m_PenTrackBorder, m_BrushTrack, mark);
+                }
+        	}
             
             if(m_SyncPointMark > 0)
             {
-            	e.Graphics.DrawLine(m_PenSyncPointMark, m_SyncPointMark, 2, m_SyncPointMark, this.Height - 4);
+            	DrawMark(e.Graphics, m_PenSyncBorder, m_PenSyncInside, m_SyncPointMark);
             }
+        }
+        private void DrawMark(Graphics _canvas, Pen _pBorder, Pen _pInside, int _iCoord)
+        {
+        	// Mark for a single point in time (key image).
+        	
+			// Simple line.
+        	//_canvas.DrawLine(_p, _iCoord, 2, _iCoord, this.Height - 4);
+        	//_canvas.DrawLine(_p, _iCoord+1, 2, _iCoord+1, this.Height - 4);
+        	
+        	// Small rectangles (Eclipse style)
+        	int iLeft = _iCoord-1;
+        	int iTop = 5;
+        	int iWidth = 3;
+        	int iHeight = 8;
+        	
+			_canvas.DrawRectangle(_pBorder, iLeft, iTop, iWidth, iHeight );
+			_canvas.DrawRectangle(_pInside, iLeft + 1, iTop+1, iWidth-2, iHeight-2 );
+        }
+        private void DrawMark(Graphics _canvas, Pen _pBorder, SolidBrush _bInside, Point _iCoords)
+        {
+        	// Mark for a range in time (chrono or tracks).
+        	int iLeft = _iCoords.X;
+        	int iTop = 5;
+        	int iWidth = _iCoords.Y;
+        	int iHeight = 8;
+        	
+			_canvas.DrawRectangle(_pBorder, iLeft, iTop, iWidth, iHeight );
+			_canvas.FillRectangle(_bInside, iLeft + 1, iTop+1, iWidth-1, iHeight-1 );
         }
         #endregion
         
@@ -271,40 +322,63 @@ namespace Kinovea.ScreenManager
             	NavCursor.Left = BumperLeft.Width + Rescale(m_iPosition - m_iMinimum, m_iMaximum - m_iMinimum, m_iMaxWidth);
             }
 
-            UpdateMarkers();
+            UpdateMarkersPositions();
         }
-       	private void UpdateMarkers()
+       	private void UpdateMarkersPositions()
         {
-       		// Key frames
-            if (m_KeyframesTimestamps != null)
-            {
-                if (m_KeyframesMarks == null)
-                {
-                    m_KeyframesMarks = new int[m_KeyframesTimestamps.Length];
-                }
-                else if (m_KeyframesTimestamps.Length != m_KeyframesMarks.Length)
-                {
-                    // dispose ?
-                    m_KeyframesMarks = new int[m_KeyframesTimestamps.Length];
-                }
+       		if(m_Metadata != null)
+       		{
+       			// Key frames
+	       		m_KeyframesMarks.Clear();
+	       		foreach(Keyframe kf in m_Metadata.Keyframes)
+	       		{
+	       			m_KeyframesMarks.Add(GetCoordFromTimestamp(kf.Position));
+	       		}
+	       		
+	       		// Chronos
+	       		m_ChronosMarks.Clear();
+	       		foreach(DrawingChrono dc in m_Metadata.Chronos)
+	       		{
+	       			if(dc.TimeStart != long.MaxValue && dc.TimeStop != long.MaxValue)
+					{
+	       				// todo: currently doesn't support the chrono without end.
+	       			
+		       			// We will store the range coords in a Point object, to get a couple of ints structure.
+		       			// X will be the left coordinate, Y the width.
+		       			int start = GetCoordFromTimestamp(dc.TimeStart);
+		       			int stop = GetCoordFromTimestamp(dc.TimeStop);
+		       			Point p = new Point(start, stop - start);
+	
+		       			m_ChronosMarks.Add(p);
+	       			}
+	       		}
+	       		
+	       		// Tracks
+	       		m_TracksMarks.Clear();
+	       		foreach(Track t in m_Metadata.Tracks)
+	       		{
+	       			// We will store the range coords in a Point object, to get a couple of ints structure.
+	       			// X will be the left coordinate, Y the width.
+	       			int start = GetCoordFromTimestamp(t.BeginTimeStamp);
+	       			int stop = GetCoordFromTimestamp(t.EndTimeStamp);
+	       			Point p = new Point(start, stop - start);
 
-                // Assign new values.
-                for (int i = 0; i < m_KeyframesTimestamps.Length; i++)
-                {
-                    m_KeyframesMarks[i] = (NavCursor.Width / 2) + BumperLeft.Width + Rescale(m_KeyframesTimestamps[i] - m_iMinimum, m_iMaximum - m_iMinimum, m_iMaxWidth);
-                }
-            }
-            
+		       		m_TracksMarks.Add(p);
+	       		}
+	       		
+       		}
+       		
             // Sync point
+            m_SyncPointMark = 0;
             if(m_SyncPointTimestamp != 0)
             {
-            	m_SyncPointMark = (NavCursor.Width / 2) + BumperLeft.Width + Rescale(m_SyncPointTimestamp - m_iMinimum, m_iMaximum - m_iMinimum, m_iMaxWidth);	
-            }
-            else
-            {
-            	m_SyncPointMark = 0;	
+            	m_SyncPointMark = GetCoordFromTimestamp(m_SyncPointTimestamp);
             }
         }
+       	private int GetCoordFromTimestamp(long _ts)
+       	{
+       		return (NavCursor.Width / 2) + BumperLeft.Width + Rescale(_ts - m_iMinimum, m_iMaximum - m_iMinimum, m_iMaxWidth);	
+       	}
         #endregion
     }
 }
