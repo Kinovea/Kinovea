6aa4a07b0c3d7ea08e891bbb72b4acbfc3130e4f
Joan Charmant <jcharmant@gmail.com>
2010-06-20 10:00:58 +0000

    Added support for rich text editing in the comment box. - bold, italic, underline and strikeout buttons and formatting. - KVA file format is modified to use an RTF string instead of a series of raw lines of text.

--- ScreenManager/PlayerScreen/UserInterface/FormKeyframeComments.cs
+++ ScreenManager/PlayerScreen/UserInterface/FormKeyframeComments.cs
@@ -18,12 +18,13 @@ along with Kinovea. If not, see http://www.gnu.org/licenses/.
 
 */
 
+using Kinovea.ScreenManager.Languages;
 using System;
+using System.Drawing;
 using System.Reflection;
 using System.Resources;
 using System.Threading;
 using System.Windows.Forms;
-
 using Kinovea.Services;
 
 namespace Kinovea.ScreenManager
@@ -49,6 +50,7 @@ namespace Kinovea.ScreenManager
         private bool m_bUserActivated;
         private Keyframe m_Keyframe;
         private PlayerScreenUserInterface m_psui;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         #endregion
 
         #region Contructors
@@ -79,12 +81,11 @@ namespace Kinovea.ScreenManager
         }
         public void RefreshUICulture()
         {
-            ResourceManager rm = new ResourceManager("Kinovea.ScreenManager.Languages.ScreenManagerLang", Assembly.GetExecutingAssembly());
-            this.Text = "   " + rm.GetString("dlgKeyframeComment_Title", Thread.CurrentThread.CurrentUICulture);
+            this.Text = "   " + ScreenManagerLang.dlgKeyframeComment_Title;
         }
         #endregion
 
-        #region Event Handlers
+        #region Form event handlers
         private void formKeyframeComments_FormClosing(object sender, FormClosingEventArgs e)
         {
             if (e.CloseReason == CloseReason.UserClosing)
@@ -109,6 +110,41 @@ namespace Kinovea.ScreenManager
         private void formKeyframeComments_MouseLeave(object sender, EventArgs e)
         {
             CheckMouseLeave();
+        }
+        #endregion
+        
+        #region Styling event handlers
+        private void btnBold_Click(object sender, EventArgs e)
+        {
+        	int style = GetSelectionStyle();
+        	style = rtbComment.SelectionFont.Bold ? style - (int)FontStyle.Bold : style + (int)FontStyle.Bold;
+        	rtbComment.SelectionFont = new Font(rtbComment.SelectionFont.FontFamily, rtbComment.SelectionFont.Size, (FontStyle)style);
+        }
+        private void btnItalic_Click(object sender, EventArgs e)
+        {
+        	int style = GetSelectionStyle();
+        	style = rtbComment.SelectionFont.Italic ? style - (int)FontStyle.Italic : style + (int)FontStyle.Italic;
+        	rtbComment.SelectionFont = new Font(rtbComment.SelectionFont.FontFamily, rtbComment.SelectionFont.Size, (FontStyle)style);
+        }
+        private void btnUnderline_Click(object sender, EventArgs e)
+        {
+        	int style = GetSelectionStyle();
+        	style = rtbComment.SelectionFont.Underline ? style - (int)FontStyle.Underline : style + (int)FontStyle.Underline;
+        	rtbComment.SelectionFont = new Font(rtbComment.SelectionFont.FontFamily, rtbComment.SelectionFont.Size, (FontStyle)style);
+        }
+        private void btnStrike_Click(object sender, EventArgs e)
+        {
+        	int style = GetSelectionStyle();
+        	style = rtbComment.SelectionFont.Strikeout ? style - (int)FontStyle.Strikeout : style + (int)FontStyle.Strikeout;
+        	rtbComment.SelectionFont = new Font(rtbComment.SelectionFont.FontFamily, rtbComment.SelectionFont.Size, (FontStyle)style);
+        }
+        private void btnForeColor_Click(object sender, EventArgs e)
+        {
+        	rtbComment.SelectionColor = Color.Red;
+        }
+        private void btnBackColor_Click(object sender, EventArgs e)
+        {
+        	rtbComment.SelectionBackColor = Color.Yellow;
         }
         #endregion
 
@@ -136,7 +172,10 @@ namespace Kinovea.ScreenManager
         private void ActivateKeyboardHandler()
         {
             // Mouse leave the info box : reactivate the keyboard handling for the screens
-            // so we can use <space>, <return>, etc. as player shortcuts
+            // so we can use <space>, <return>, etc. as player shortcuts.
+            // This is sometimes strange. You put the mouse away to start typing, 
+            // and the first carriage return triggers the playback leaving the key image.
+            
             DelegatesPool dp = DelegatesPool.Instance();
             if (dp.ActivateKeyboardHandler != null)
             {
@@ -148,22 +187,17 @@ namespace Kinovea.ScreenManager
             // Update
             txtTitle.Text = m_Keyframe.Title;
             rtbComment.Clear();
-            foreach (string line in m_Keyframe.Comments)
-            {
-                rtbComment.AppendText(line);
-            }
+            rtbComment.Rtf = m_Keyframe.CommentRtf;
         }
         private void SaveInfos()
         {
             // Commit changes to the keyframe
             // This must not be called at each info modification otherwise the update routine breaks...
+            
+            log.Debug("Saving comment and title");
             if (m_Keyframe != null)
             {
-	            m_Keyframe.Comments.Clear();
-                foreach (string line in rtbComment.Lines)
-                {
-                    m_Keyframe.Comments.Add(line + "\n");
-                }
+	            m_Keyframe.CommentRtf = rtbComment.Rtf;
 	
             	if(m_Keyframe.Title != txtTitle.Text)
             	{
@@ -172,11 +206,33 @@ namespace Kinovea.ScreenManager
             	}
             }
         }
+        private int GetSelectionStyle()
+        {
+        	// Combine all the styles into an int, to have generic toggles methods.
+        	int bold = rtbComment.SelectionFont.Bold ? (int)FontStyle.Bold : 0;
+        	int italic = rtbComment.SelectionFont.Italic ? (int)FontStyle.Italic : 0;
+        	int underline = rtbComment.SelectionFont.Underline ? (int)FontStyle.Underline : 0;
+        	int strikeout = rtbComment.SelectionFont.Strikeout ? (int)FontStyle.Strikeout : 0;
+        	
+        	return bold + italic + underline + strikeout;
+        }
+        private void LogCurrentSelection()
+        {
+        	log.Debug(String.Format("Selection font name:{0}", rtbComment.SelectionFont.Name));
+        	log.Debug(String.Format("Selection font size:{0}", rtbComment.SelectionFont.Size));
+        	log.Debug(String.Format("Selection font bold:{0}", rtbComment.SelectionFont.Bold));
+        	log.Debug(String.Format("Selection font italic:{0}", rtbComment.SelectionFont.Italic));
+        	log.Debug(String.Format("Selection font underline:{0}", rtbComment.SelectionFont.Underline));
+        	log.Debug(String.Format("Selection font strikeout:{0}", rtbComment.SelectionFont.Strikeout));
+        	
+        	log.Debug(String.Format("Selection back color: {0}", rtbComment.SelectionBackColor.ToString()));
+        	log.Debug(String.Format("Selection fore color: {0}", rtbComment.SelectionColor.ToString()));
+        	
+        	/*log.Debug(String.Format("regular : {0}, bold : {1}, italic : {2}, underline : {3}, strikeout : {4}", 
+        	                        (int)FontStyle.Regular, (int)FontStyle.Bold, (int)FontStyle.Italic, (int)FontStyle.Underline, (int)FontStyle.Strikeout));
+        	log.Debug(String.Format("style : {0}", rtbComment.SelectionFont.Style));*/
+        }
         #endregion
-
-       
-
-        
 
     }
 }
