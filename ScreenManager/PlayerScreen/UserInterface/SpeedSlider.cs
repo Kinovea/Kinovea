43f6ace3661d9160fc169b2f0b08aefca098aa10
joan <joan@2c3a7681-93f5-4a91-9fda-68cc05e6b8ad>
2010-10-12 22:13:42 +0000

    Refactoring in the speed slider control, now completely drawn in its paint method. This should fix this control's UI issues on Win 7 High DPI.

--- ScreenManager/PlayerScreen/UserInterface/SpeedSlider.cs
+++ ScreenManager/PlayerScreen/UserInterface/SpeedSlider.cs
@@ -28,6 +28,20 @@ using AForge.Imaging.Filters;
 
 namespace Kinovea.ScreenManager
 {
+	/// A slider control.
+	/// 
+	/// When value is modified by user:
+	/// - The internal value is modified.
+	/// - Events are raised, which are listened to by parent control.
+	/// - Parent control update its own internal data state by reading the properties.
+	/// 
+	/// When control appearence needs to be updated
+	/// - This is when internal data of the parent control have been modified by other means.
+	/// - (At initialization for example)
+	/// - The public properties setters are provided, they doesn't raise the events back.
+	/// 
+	/// This control is pretty similar to FrameTracker. Maybe it would be possible to factorize the code.
+	/// </summary>
     public partial class SpeedSlider : UserControl
     {
         #region Properties
@@ -65,17 +79,11 @@ namespace Kinovea.ScreenManager
             }
             set
             {
-                if (value < m_iMinimum) value = m_iMinimum;
-                if (value > m_iMaximum) value = m_iMaximum;
-
                 m_iValue = value;
-
-                UpdateAppearance();
-                
-                if (ValueChanged != null) 
-	            { 
-	                ValueChanged(this, EventArgs.Empty);
-	            }
+                if (m_iValue < m_iMinimum) m_iValue = m_iMinimum;
+                if (m_iValue > m_iMaximum) m_iValue = m_iMaximum;
+                UpdateCursorPosition();
+				Invalidate();				
             }
         }
         [Category("Misc"), Browsable(true)]
@@ -83,8 +91,7 @@ namespace Kinovea.ScreenManager
         {
             set
             {
-                toolTips.SetToolTip(btnCursor, value);
-                toolTips.SetToolTip(btnRail, value);
+                toolTips.SetToolTip(this, value);
             }
         }
         [Category("Behavior"), Browsable(true)]
@@ -123,155 +130,257 @@ namespace Kinovea.ScreenManager
                 m_iStickyValue = value;
             }
         }
+        [Category("Behavior"), Browsable(true)]
+        public bool MiddleMark
+        {
+            get
+            {
+                return m_bMiddleMark;
+            }
+            set
+            {
+                m_bMiddleMark = value;
+            }
+        }
         #endregion
 
         #region Members
+        private bool m_bInvalidateAsked;	// To prevent reentry in MouseMove before the paint event has been honored.	
+        private bool m_bEnabled = true;
+        
         private int m_iMinimum = 1;
         private int m_iValue = 100;
         private int m_iMaximum = 200;
         private int m_iSmallChange = 1;
         private int m_iLargeChange = 5;
         private int m_iStickyValue = 100;
-        private Bitmap m_CursorDisabled;
+        
+        private int m_iMaxWidth;			// Number of pixels in the control that can be used for values.
+        private int m_iMinimumPixel;
+        private int m_iMaximumPixel;
+        private int m_iMiddlePixel;
+        private int m_iPixelPosition;		// Left of the cursor in pixels.
+        private bool m_bMiddleMark = true;
+        
+        private bool m_bDecreasing;
+		private bool m_bIncreasing;
+		
+        private int m_iSpacers = 10;			// size of space between buttons and rail.
+        private int m_iButtonWidth = Resources.SpeedTrkDecrease2.Width;
+        private int m_iCursorWidth = Resources.SpeedTrkCursor7.Width;
+        
+        private Bitmap bmpDecrease = Resources.SpeedTrkDecrease2;
+        private Bitmap bmpIncrease = Resources.SpeedTrkIncrease2;
+        private Bitmap bmpBackground = Resources.SpeedTrkBack5;
+        private Bitmap bmpCursor = Resources.SpeedTrkCursor7;
+        
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         #endregion
 
         #region EventDelegates
-        // Déclarations de Types
         public delegate void ValueChangedHandler(object sender, EventArgs e);
-
-        // Déclarations des évènements
+        
         [Category("Action"), Browsable(true)]
         public event ValueChangedHandler ValueChanged;
-
         #endregion
 
         #region Ctor
         public SpeedSlider()
         {
             InitializeComponent();
+            
+            // Activates double buffering
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
+            
+            this.Cursor = Cursors.Hand;
+            
+            m_iMinimumPixel = m_iButtonWidth + m_iSpacers;
+            m_iMaximumPixel = this.Width - (m_iSpacers + m_iButtonWidth);
+            m_iMaxWidth = m_iMaximumPixel - m_iMinimumPixel;
+            m_iMiddlePixel = m_iMinimumPixel + (m_iMaxWidth / 2);
+                        
             this.BackColor = Color.White;
-            m_CursorDisabled = Grayscale.CommonAlgorithms.Y.Apply(Resources.SpeedTrkCursor7);
         }
         #endregion
 
-        #region Internal Events
-        private void btnCursor_MouseMove(object sender, MouseEventArgs e)
+        #region Public Methods
+        public void EnableDisable(bool _bEnable)
         {
-            if (e.Button == MouseButtons.Left)
-            {
-                int iTargetPosition = btnCursor.Left + e.X;
-                if((iTargetPosition > btnRail.Left) && (iTargetPosition <= btnRail.Left + btnRail.Width))
-                {
-                    btnCursor.Left = iTargetPosition - (btnCursor.Width / 2);
-                    SetNewValue();
-                }
-            }
-        }
-        private void btnRail_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                btnCursor.Left = btnRail.Left + e.X - (btnCursor.Width / 2);
-                SetNewValue();
-            }
-        }
-        private void btnRail_MouseMove(object sender, MouseEventArgs e)
-        {
-            if ((e.Button == MouseButtons.Left) && (e.X > 0) && (e.X <= btnRail.Width))
-            {
-                SuspendLayout();
-                btnCursor.Left = btnRail.Left + e.X - (btnCursor.Width / 2);
-                ResumeLayout();
-                SetNewValue();
-            }
-        }
-        private void btnDecrease_MouseClick(object sender, MouseEventArgs e)
-        {
-            // -> Déplacer le curseur.
-            if (m_iMaximum - m_iMinimum > 0 && m_iValue > m_iMinimum + m_iLargeChange)
-            {
-                m_iValue -= m_iLargeChange;
-
-                btnCursor.Left = btnRail.Left + Rescale(m_iValue - m_iMinimum, m_iMaximum - m_iMinimum, btnRail.Width);
-
-                if (ValueChanged != null)
-                {
-                    ValueChanged(this, EventArgs.Empty);
-                }
-            }
-        }
-        private void btnIncrease_MouseClick(object sender, MouseEventArgs e)
-        {
-            // -> Déplacer le curseur.
-            if (m_iMaximum - m_iMinimum > 0 && m_iValue <= m_iMaximum - m_iLargeChange)
-            {
-                m_iValue += m_iLargeChange;
-
-                btnCursor.Left = btnRail.Left + Rescale(m_iValue - m_iMinimum, m_iMaximum - m_iMinimum, btnRail.Width);
-
-                if (ValueChanged != null)
-                {
-                    ValueChanged(this, EventArgs.Empty);
-                }
-            }
+        	m_bEnabled = _bEnable;
+			Invalidate();
         }
         #endregion
         
-        public void EnableDisable(bool _bEnable)
+        #region Event Handlers - User Manipulation
+        private void SpeedSlider_MouseDown(object sender, MouseEventArgs e)
         {
-        	btnIncrease.Enabled = _bEnable;
-        	btnDecrease.Enabled = _bEnable;
-        	btnCursor.Enabled = _bEnable;
-        	btnCursor.BackgroundImage = _bEnable ? Resources.SpeedTrkCursor7 : m_CursorDisabled;
-        }
-        public void UpdateSpeedValue(int _value)
-        {	
-        	// This method is called when programmatically changing speed value,
-        	// and we don't want to raise the event back.
+        	// Register which button we hit. We'll handle the action in MouseUp.
         	
-			if (_value < m_iMinimum) _value = m_iMinimum;
-            if (_value > m_iMaximum) _value = m_iMaximum;
-
-            m_iValue = _value;
-            
-            UpdateAppearance();
-        }
-        private void SetNewValue()
-        {
-        	// This method is called when the user changes the speed from the control.
-            m_iValue = Rescale(btnCursor.Left + (btnCursor.Width / 2) - btnRail.Left, btnRail.Width, (m_iMaximum - m_iMinimum));
-
-            m_iValue += m_iMinimum;
-            
-            // Stickiness
-            if (
-                (m_iValue >= (m_iStickyValue - (2*m_iSmallChange))) && (m_iValue <= m_iStickyValue) ||
-                (m_iValue <= (m_iStickyValue + (2*m_iSmallChange))) && (m_iValue >= m_iStickyValue)
-                )
+        	m_bDecreasing = false;
+        	m_bIncreasing = false;
+        	
+        	if (m_bEnabled && e.Button == MouseButtons.Left)
             {
-                m_iValue = m_iStickyValue;
-            }
-            
-            if (ValueChanged != null) 
-            { 
-                ValueChanged(this, EventArgs.Empty);
-            }
+        		if(e.X >= 0 && e.X < m_iButtonWidth)
+        		{
+        			// on decrease button.
+        			m_bDecreasing = true;
+        		}
+        		else if(e.X >= Width - m_iButtonWidth && e.X < Width)
+        		{
+        			// on increase button.
+        			m_bIncreasing = true;
+        		}
+        	}
         }
-        private void UpdateAppearance()
+        private void SpeedSlider_MouseMove(object sender, MouseEventArgs e)
         {
-        	// This method is called when programmatically changing the speed.
-            if (m_iMaximum - m_iMinimum > 0)
+        	// Note: also raised on mouse down.
+        	// User wants to jump to position.
+        	if(m_bEnabled && !m_bInvalidateAsked)
+        	{
+	        	if (e.Button == MouseButtons.Left)
+	            {
+	        		Point mouseCoords = this.PointToClient(Cursor.Position);
+	        		
+	        		if ((mouseCoords.X > m_iMinimumPixel) && (mouseCoords.X < m_iMaximumPixel))
+	                {
+	        			m_iPixelPosition = mouseCoords.X - (m_iCursorWidth/2);
+            			m_iValue = GetValueFromCoord(mouseCoords.X);
+            			
+			            // Stickiness
+			            if (
+			                (m_iValue >= (m_iStickyValue - 5)) && (m_iValue <= m_iStickyValue) ||
+			                (m_iValue <= (m_iStickyValue + 5)) && (m_iValue >= m_iStickyValue)
+			                )
+			            {
+			            	// Inside sticky zone, fall back to sticky value.
+			            	m_iValue = m_iStickyValue;
+			            	m_iPixelPosition = m_iMiddlePixel - (m_iCursorWidth/2);
+			            }
+	        		
+	        			Invalidate();
+					    m_bInvalidateAsked = true;
+					    
+						if (ValueChanged != null) 
+			            { 
+			                ValueChanged(this, EventArgs.Empty);
+			            }
+	        		}
+	        	}
+        	}
+        }
+        private void SpeedSlider_MouseUp(object sender, MouseEventArgs e)
+        {
+        	// This is when the validation of the change occur.
+        	if (m_bEnabled && e.Button == MouseButtons.Left)
             {
-                SuspendLayout();
-                btnCursor.Left = btnRail.Left + Rescale(m_iValue - m_iMinimum, m_iMaximum - m_iMinimum, btnRail.Width);
-                ResumeLayout();
-            }            
+        		bool changed = false;
+        		
+        		if(m_bDecreasing)
+        		{
+	        		if (m_iMaximum - m_iMinimum > 0 && m_iValue > m_iMinimum + m_iLargeChange)
+		            {
+		                m_iValue -= m_iLargeChange;
+		        		changed = true;
+		            }
+        		}
+        		else if(m_bIncreasing)
+        		{
+        			if (m_iMaximum - m_iMinimum > 0 && m_iValue <= m_iMaximum - m_iLargeChange)
+        			{
+        				m_iValue += m_iLargeChange;
+        				changed = true;
+        			}
+				}
+        		
+        		if(changed)
+        		{
+        			UpdateCursorPosition();
+        			Invalidate();
+    			    if (ValueChanged != null)
+	                {
+	                    ValueChanged(this, EventArgs.Empty);
+	                }
+        		}
+        	}
         }
-        private int Rescale(long _iValue, long _iOldMax, long _iNewMax)
+        #endregion
+        
+        #region Paint / Resize
+        private void SpeedSlider_Paint(object sender, PaintEventArgs e)
         {
-            // Rescale : Pixels -> Values
-            return (int)(Math.Round((double)((double)_iValue * (double)_iNewMax) / (double)_iOldMax));
-        }  
+        	// When we land in this function, m_iPixelPosition should have been set already.
+        	// It is the only member variable we'll use here.
+        	
+        	// Draw buttons
+        	if(m_bEnabled)
+        	{
+        		e.Graphics.DrawImage(bmpDecrease, 0, 0);
+        		e.Graphics.DrawImage(bmpIncrease, m_iMaximumPixel + m_iSpacers, 0);
+        	}
+        	
+        	// Draw tiled background
+        	for(int i=m_iMinimumPixel;i<m_iMaximumPixel; i+=bmpBackground.Width)
+        	{
+        		e.Graphics.DrawImage(bmpBackground, i, 0);
+        	}
+        	
+        	// MiddleMarker
+        	if(m_bMiddleMark)
+        	{
+        		e.Graphics.DrawLine(Pens.Gray, m_iMiddlePixel, 0, m_iMiddlePixel, 3);
+        		e.Graphics.DrawLine(Pens.Gray, m_iMiddlePixel, 7, m_iMiddlePixel, 10);
+        	}
+        	
+      		// Draw th e cursor.
+        	if(m_bEnabled)
+        	{
+        	    e.Graphics.DrawImage(bmpCursor, m_iPixelPosition, 0);
+        	}
+        	
+        	m_bInvalidateAsked = false;
+        }
+        private void SpeedSlider_Resize(object sender, EventArgs e)
+        {
+        	// Resize of the control only : internal data doesn't change.
+        	m_iMaximumPixel = this.Width - (m_iSpacers + m_iButtonWidth);
+            m_iMaxWidth = m_iMaximumPixel - m_iMinimumPixel;
+            m_iMiddlePixel = m_iMinimumPixel + (m_iMaxWidth / 2);
+            
+            UpdateCursorPosition();
+            Invalidate();
+        }
+        #endregion
+        
+        #region Binding UI to Data
+        private void UpdateCursorPosition()
+        {
+        	// This method updates the appearence of the control only, it doesn't raise the events back.
+        	// Should be called every time m_iPosition has been updated. 
+            m_iPixelPosition = GetCoordFromValue(m_iValue) - (m_iCursorWidth/2);
+        }
+        private int GetCoordFromValue(int _value)
+       	{
+			int iret = m_iMinimumPixel + Rescale(_value - m_iMinimum, m_iMaximum - m_iMinimum, m_iMaxWidth);
+            return iret;
+       	}
+        private int GetValueFromCoord(int _pos)
+       	{
+       		int ret = m_iMinimum + Rescale(_pos - m_iMinimumPixel, m_iMaxWidth, m_iMaximum - m_iMinimum);
+       		return ret;
+       	}
+        private int Rescale(long _iOldValue, long _iOldMax, long _iNewMax)
+        {
+            if(_iOldMax > 0)
+            {
+            	return (int)(Math.Round((double)((double)_iOldValue * (double)_iNewMax) / (double)_iOldMax));
+            }
+            else
+            {
+            	return 0;
+            }
+        }
+        #endregion
     }
 }
