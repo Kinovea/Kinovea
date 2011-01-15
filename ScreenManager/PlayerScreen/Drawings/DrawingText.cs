d5afbec54d64f658dd2b5392d0e5f9e93f676564
joan <joan@2c3a7681-93f5-4a91-9fda-68cc05e6b8ad>
2011-01-15 18:00:13 +0000

    Refactoring impacting all drawing tools using text. The idea was first to factor the background drawing in a single class (new class LabelBackground). Then to homogenize the various drawing tools using text, especially those using the KeyframeLabel class (line for the measure, track). The mini label in KeyframeLabel is now always in absolute coordinates.
    Fix bug 230 - Speed label too small for some videos.
    The font size is now always floored to 8.

--- ScreenManager/PlayerScreen/Drawings/DrawingText.cs
+++ ScreenManager/PlayerScreen/Drawings/DrawingText.cs
@@ -1,5 +1,6 @@
+#region License
 /*
-Copyright © Joan Charmant 2008.
+Copyright © Joan Charmant 2008-2011.
 joan.charmant@gmail.com 
  
 This file is part of Kinovea.
@@ -17,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with Kinovea. If not, see http://www.gnu.org/licenses/.
 
 */
+#endregion
 
 using System;
 using System.Drawing;
@@ -73,7 +75,7 @@ namespace Kinovea.ScreenManager
                     }
 
                     m_TextBox.Text = m_Text;
-                    m_TextBox.Location = new Point(m_ContainerScreen.Left + m_RescaledBackground.X + 6, m_ContainerScreen.Top + m_RescaledBackground.Y + 3);
+                    m_TextBox.Location = new Point(m_ContainerScreen.Left + m_LabelBackground.Location.X + 6, m_ContainerScreen.Top + m_LabelBackground.Location.Y + 3);
                     AutoSizeTextbox();
                     m_TextBox.Visible = true;
                     m_TextBox.Focus();
@@ -102,20 +104,21 @@ namespace Kinovea.ScreenManager
         #region Members
         private string m_Text;							// Actual text displayed.
         private Color m_TextboxColor;					// Color of the textbox (?)
-        private Rectangle m_BackgroundRectangle;		// Sizes of the background. Computed from text length.
         
       	private InfosTextDecoration m_TextStyle;		// Style infos (font, font size, colors)
         private InfosTextDecoration m_MemoTextStyle;	// Used when configuring.
       	private InfosFading m_InfosFading;
         
       	private bool m_bEditMode;
-      	
-      	private static readonly int m_iDefaultBackgroundAlpha = 128;
       	private static readonly int m_iDefaultFontSize = 16;    		// will also be used for the text box.
 		
+      	private Point m_TopLeft;                         	// position (in image coords).
         private double m_fStretchFactor;
         private Point m_DirectZoomTopLeft;
-        private Rectangle m_RescaledBackground;
+        
+        private LabelBackground m_LabelBackground = new LabelBackground();
+        private SizeF m_BackgroundSize;				  	// Size of the background rectangle (scaled).
+        
         private TextBox m_TextBox;
         private PictureBox m_ContainerScreen;
         #endregion
@@ -125,7 +128,9 @@ namespace Kinovea.ScreenManager
         {
             m_Text = " ";
             m_TextboxColor = Color.White; // Color.LightSteelBlue;
-            m_BackgroundRectangle = new Rectangle(x, y, width, height);            
+            m_TopLeft = new Point(x, y);
+            m_BackgroundSize = new SizeF(100, 20);
+            
             m_TextStyle = new InfosTextDecoration("Arial", m_iDefaultFontSize, FontStyle.Bold, Color.White, Color.CornflowerBlue);
 
             m_InfosFading = new InfosFading(_iTimestamp, _iAverageTimeStampsPerFrame);
@@ -167,7 +172,7 @@ namespace Kinovea.ScreenManager
                     DrawBackground(_canvas, fOpacityFactor);
                     SolidBrush fontBrush = new SolidBrush(m_TextStyle.GetFadingForeColor(fOpacityFactor));
                     Font fontText = m_TextStyle.GetInternalFont((float)m_fStretchFactor);
-                    _canvas.DrawString(m_Text, fontText, fontBrush, new Point(m_RescaledBackground.X + 5, m_RescaledBackground.Y + 3));
+                   _canvas.DrawString(m_Text, fontText, fontBrush, m_LabelBackground.TextLocation);
                 }
             }
         }
@@ -193,31 +198,27 @@ namespace Kinovea.ScreenManager
             return iHitResult;
         }
         public override void MoveHandleTo(Point point, int handleNumber)
-        {
-            // Invisible handler to change font size.
+        {	
+        	// Invisible handler to change font size.
             // Compare wanted mouse position with current bottom right.
-            int wantedHeight = point.Y - m_BackgroundRectangle.Top;
+            int wantedHeight = point.Y - m_TopLeft.Y;
             int newFontSize = m_TextStyle.ReverseFontSize(wantedHeight, m_Text);
             UpdateDecoration(newFontSize);
         }
         public override void MoveDrawing(int _deltaX, int _deltaY)
         {
-            // TODO : image width & height
-            if (m_BackgroundRectangle.X + _deltaX >= 0 && m_BackgroundRectangle.Y + _deltaY >= 0  )
+            // Note: _delatX and _delatY are mouse delta already descaled.            
+            m_TopLeft.X += _deltaX;
+            m_TopLeft.Y += _deltaY;
+
+            if (m_bEditMode && m_TextBox != null)
             {
-               // _delatX and _delatY are mouse delta descaled.
-                m_BackgroundRectangle = new Rectangle(m_BackgroundRectangle.X + _deltaX, m_BackgroundRectangle.Y + _deltaY, m_BackgroundRectangle.Width, m_BackgroundRectangle.Height);
-
-                // move TextBox
-                if (m_bEditMode && m_TextBox != null)
-                {
-                    m_TextBox.Top += _deltaY;
-                    m_TextBox.Left += _deltaX;
-                }
-
-                // Update scaled coordinates accordingly.
-                RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
+                m_TextBox.Top += _deltaY;
+                m_TextBox.Left += _deltaX;
             }
+            
+            // Update scaled coordinates accordingly.
+            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
         }
         public override void ToXmlString(XmlTextWriter _xmlWriter)
         {
@@ -231,7 +232,7 @@ namespace Kinovea.ScreenManager
 
             // Background StartPoint
             _xmlWriter.WriteStartElement("Position");
-            _xmlWriter.WriteString(m_BackgroundRectangle.Left.ToString() + ";" + m_BackgroundRectangle.Top.ToString());
+            _xmlWriter.WriteString(m_TopLeft.X.ToString() + ";" + m_TopLeft.Y.ToString());
             _xmlWriter.WriteEndElement();
 
             // Textbox Color
@@ -282,8 +283,7 @@ namespace Kinovea.ScreenManager
             // Combine all relevant fields with XOR to get the Hash.
             int iHash = m_Text.GetHashCode();
             iHash ^= m_TextboxColor.GetHashCode();
-            iHash ^= m_BackgroundRectangle.GetHashCode();
-            iHash ^= m_iDefaultBackgroundAlpha.GetHashCode();
+            iHash ^= m_TopLeft.GetHashCode();
             iHash ^= m_TextStyle.GetHashCode();
             return iHash;
         }
@@ -307,7 +307,7 @@ namespace Kinovea.ScreenManager
                         //dt.Background.Location = new Point(plotGrid.X, plotGrid.Y);
 
                         // Adapt to new Image size.
-                        dt.m_BackgroundRectangle.Location = new Point((int)((float)p.X * _scale.X), (int)((float)p.Y * _scale.Y));
+                        dt.m_TopLeft = new Point((int)((float)p.X * _scale.X), (int)((float)p.Y * _scale.Y));
                     }
                     else if (_xmlReader.Name == "TextDecoration")
                     {
@@ -343,7 +343,7 @@ namespace Kinovea.ScreenManager
             m_DirectZoomTopLeft = new Point(_DirectZoomTopLeft.X, _DirectZoomTopLeft.Y);
 
             RescaleCoordinates(_fStretchFactor, _DirectZoomTopLeft);
-            m_TextBox.Location = new Point(m_ContainerScreen.Left + m_RescaledBackground.X + 6, m_ContainerScreen.Top + m_RescaledBackground.Y + 3);
+            m_TextBox.Location = new Point(m_ContainerScreen.Left + m_LabelBackground.Location.X + 6, m_ContainerScreen.Top + m_LabelBackground.Location.Y + 3);
         }
 
         #region Lower level helpers
@@ -354,37 +354,33 @@ namespace Kinovea.ScreenManager
         }
         private void RescaleCoordinates(double _fStretchFactor, Point _DirectZoomTopLeft)
         {
-            m_RescaledBackground = new Rectangle();
-            m_RescaledBackground.Location = new Point((int)((double)(m_BackgroundRectangle.X-_DirectZoomTopLeft.X) * _fStretchFactor), (int)((double)(m_BackgroundRectangle.Y-_DirectZoomTopLeft.Y) * _fStretchFactor));
-            m_RescaledBackground.Size = new Size((int)((double)m_BackgroundRectangle.Width * _fStretchFactor), (int)((double)m_BackgroundRectangle.Height * _fStretchFactor));
+        	m_LabelBackground.Location = new Point((int)((double)(m_TopLeft.X-_DirectZoomTopLeft.X) * _fStretchFactor), (int)((double)(m_TopLeft.Y-_DirectZoomTopLeft.Y) * _fStretchFactor));
+        }
+        private void ShiftCoordinates(int _iShiftHorz, int _iShiftVert, double _fStretchFactor)
+        {
+            m_LabelBackground.Location = new Point((int)((double)m_TopLeft.X * _fStretchFactor) + _iShiftHorz, (int)((double)m_TopLeft.Y * _fStretchFactor) + _iShiftVert);
         }
         private bool IsPointInObject(Point _point)
         {
             // Point coordinates are descaled.
-            
-            // Create path which contains wide line for easy mouse selection
+            // We need to descale the hit area size for coherence.
+            Size descaledSize = new Size((int)((m_BackgroundSize.Width + m_LabelBackground.MarginWidth) / m_fStretchFactor), (int)((m_BackgroundSize.Height + m_LabelBackground.MarginHeight) / m_fStretchFactor));
+
             GraphicsPath areaPath = new GraphicsPath();
-            areaPath.AddRectangle(m_BackgroundRectangle);           
+            areaPath.AddRectangle(new Rectangle(m_TopLeft.X, m_TopLeft.Y, descaledSize.Width, descaledSize.Height));
 
             // Create region from the path
             Region areaRegion = new Region(areaPath);
 
-            bool hit =  areaRegion.IsVisible(_point);
+            bool hit = areaRegion.IsVisible(_point);
             return hit;
         }
         private void AutoSizeTextbox()
         {
             // We add a space at the end for when there is a new line with no characters in it.
-
             Button but = new Button();
             Graphics g = but.CreateGraphics();
-            
-            //Size of background
-            SizeF bgSize = g.MeasureString(m_Text + " ", m_TextStyle.GetInternalFont());
-            m_BackgroundRectangle.Width = (int)bgSize.Width + 8;
-            m_BackgroundRectangle.Height = (int)bgSize.Height + 4;
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
-
+           
             // Size of textbox, we don't use the actual font size (far too big)
             Font f = m_TextStyle.GetInternalFont();
             SizeF edSize = g.MeasureString(m_Text + " ", new Font(f.Name, m_iDefaultFontSize, f.Style));
@@ -396,39 +392,19 @@ namespace Kinovea.ScreenManager
         {
             // Draw background (Rounded rectangle)
             // The radius for rounding is based on font size.
-            double fFontSize = (double)m_TextStyle.FontSize * m_fStretchFactor;
-            int radius = (int)(fFontSize / 2);
-            int diameter = radius * 2;
-
-            GraphicsPath gp = new GraphicsPath();
-            gp.StartFigure();
-
-            gp.AddArc(m_RescaledBackground.X, m_RescaledBackground.Y, diameter, diameter, 180, 90);
-            gp.AddLine(m_RescaledBackground.X + radius, m_RescaledBackground.Y, m_RescaledBackground.X + m_RescaledBackground.Width - diameter, m_RescaledBackground.Y);
-
-            gp.AddArc(m_RescaledBackground.X + m_RescaledBackground.Width - diameter, m_RescaledBackground.Y, diameter, diameter, 270, 90);
-            gp.AddLine(m_RescaledBackground.X + m_RescaledBackground.Width, m_RescaledBackground.Y + radius, m_RescaledBackground.X + m_RescaledBackground.Width, m_RescaledBackground.Y + m_RescaledBackground.Height - diameter);
-
-            gp.AddArc(m_RescaledBackground.X + m_RescaledBackground.Width - diameter, m_RescaledBackground.Y + m_RescaledBackground.Height - diameter, diameter, diameter, 0, 90);
-            gp.AddLine(m_RescaledBackground.X + m_RescaledBackground.Width - radius, m_RescaledBackground.Y + m_RescaledBackground.Height, m_RescaledBackground.X + radius, m_RescaledBackground.Y + m_RescaledBackground.Height);
-
-            gp.AddArc(m_RescaledBackground.X, m_RescaledBackground.Y + m_RescaledBackground.Height - diameter, diameter, diameter, 90, 90);
-            gp.AddLine(m_RescaledBackground.X, m_RescaledBackground.Y + m_RescaledBackground.Height - radius, m_RescaledBackground.X, m_RescaledBackground.Y + radius);
-
-            gp.CloseFigure();
-
-            _canvas.FillPath(new SolidBrush(Color.FromArgb((int)((double)m_iDefaultBackgroundAlpha * _fOpacityFactor), m_TextStyle.BackColor)), gp);
+            Font f = m_TextStyle.GetInternalFont((float)m_fStretchFactor);
+            m_BackgroundSize = _canvas.MeasureString(m_Text + " ", f);
+            int radius = (int)(f.Size / 2);
+            
+            m_LabelBackground.Draw(_canvas, _fOpacityFactor, radius, (int)m_BackgroundSize.Width, (int)m_BackgroundSize.Height, m_TextStyle.BackColor);
         }
-        private void ShiftCoordinates(int _iShiftHorz, int _iShiftVert, double _fStretchFactor)
-        {
-            m_RescaledBackground = new Rectangle();
-            m_RescaledBackground.Location = new Point((int)((double)m_BackgroundRectangle.X * _fStretchFactor) + _iShiftHorz, (int)((double)m_BackgroundRectangle.Y * _fStretchFactor) + _iShiftVert);
-            m_RescaledBackground.Size = new Size(m_BackgroundRectangle.Width, m_BackgroundRectangle.Height);
-        }
+        
         private Rectangle GetHandleRectangle()
         {
             // This function is only used for Hit Testing.
-            return new Rectangle(m_BackgroundRectangle.Right - 10, m_BackgroundRectangle.Bottom - 10, 20, 20);
+            Size descaledSize = new Size((int)((m_BackgroundSize.Width + m_LabelBackground.MarginWidth) / m_fStretchFactor), (int)((m_BackgroundSize.Height + m_LabelBackground.MarginHeight) / m_fStretchFactor));
+
+            return new Rectangle(m_TopLeft.X + descaledSize.Width - 10, m_TopLeft.Y + descaledSize.Height - 10, 20, 20);
         }
         #endregion
 
