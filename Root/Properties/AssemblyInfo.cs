58d172fe8fbea74da4edaaff29619b0c24fb60e7
Joan Charmant <jcharmant@gmail.com>
2011-04-01 14:31:38 +0000

    Fix several issues related to canceling the close of player screen.

--- Root/Properties/AssemblyInfo.cs
+++ Root/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.8.14.*")]
-[assembly: AssemblyFileVersion("0.8.14.0")]
+[assembly: AssemblyVersion("0.8.15.*")]
+[assembly: AssemblyFileVersion("0.8.15.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
