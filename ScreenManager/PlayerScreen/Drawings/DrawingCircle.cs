cf63ac35800bd98e97d6f92cf0b006fdeb0b6a4b
joan <joan@2c3a7681-93f5-4a91-9fda-68cc05e6b8ad>
2011-08-13 11:31:09 +0000

    [Refactoring] - Circle, Cross and Line to use CoordinateSystem. + New extension method for inverted color.

--- ScreenManager/PlayerScreen/Drawings/DrawingCircle.cs
+++ ScreenManager/PlayerScreen/Drawings/DrawingCircle.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿#region License
+/*
 Copyright © Joan Charmant 2010.
 joan.charmant@gmail.com 
  
@@ -17,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with Kinovea. If not, see http://www.gnu.org/licenses/.
 
 */
+#endregion
 
 using System;
 using System.Collections.Generic;
@@ -59,35 +61,23 @@ namespace Kinovea.ScreenManager
 
         #region Members
         // Core
-        private Point m_Origin;
+        private Point m_Center;
         private int m_iRadius;
+        private bool m_bSelected;
+        // Decoration
         private StyleHelper m_StyleHelper = new StyleHelper();
         private DrawingStyle m_Style;
         private InfosFading m_InfosFading;
-        private double m_fStretchFactor;
-        private Point m_DirectZoomTopLeft;
-        private bool m_bSelected;
-
-        // Computed
-        private int m_iDiameter;
-        private Point m_RescaledOrigin;
-        private int m_iRescaledRadius;
-        private int m_iRescaledDiameter;
         
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         #endregion
 
         #region Constructor
-        public DrawingCircle(int Ox, int Oy, int radius, long _iTimestamp, long _iAverageTimeStampsPerFrame, DrawingStyle _preset)
+        public DrawingCircle(Point _center, int radius, long _iTimestamp, long _iAverageTimeStampsPerFrame, DrawingStyle _preset)
         {
-            // Core
-            m_Origin = new Point(Ox, Oy);
-            m_iRadius = radius;
-            if(m_iRadius < 10) m_iRadius = 10;
-            m_iDiameter = m_iRadius * 2;
-            m_fStretchFactor = 1.0;
-            m_DirectZoomTopLeft = new Point(0, 0);
-			m_InfosFading = new InfosFading(_iTimestamp, _iAverageTimeStampsPerFrame);
+            m_Center = _center;
+            m_iRadius = Math.Min(radius, 10);
+            m_InfosFading = new InfosFading(_iTimestamp, _iAverageTimeStampsPerFrame);
             
 			m_StyleHelper.Color = Color.Empty;
 			m_StyleHelper.LineSize = 1;
@@ -96,12 +86,9 @@ namespace Kinovea.ScreenManager
                 m_Style = _preset.Clone();
                 BindStyle();
 			}
-            
-            // Computed
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
         }
         public DrawingCircle(XmlReader _xmlReader, PointF _scale, Metadata _parent)
-            : this(0,0,0,0,0, ToolManager.Circle.StylePreset.Clone())
+            : this(Point.Empty,0,0,0, ToolManager.Circle.StylePreset.Clone())
         {
             ReadXml(_xmlReader, _scale);
         }
@@ -111,70 +98,50 @@ namespace Kinovea.ScreenManager
         public override void Draw(Graphics _canvas, CoordinateSystem _transformer, double _fStretchFactor, bool _bSelected, long _iCurrentTimestamp, Point _DirectZoomTopLeft)
         {
             double fOpacityFactor = m_InfosFading.GetOpacityFactor(_iCurrentTimestamp);
-            int iPenAlpha = (int)((double)255 * fOpacityFactor);
-			m_bSelected = _bSelected;
+            if(fOpacityFactor <= 0)
+                return;
+            
+            int alpha = (int)(fOpacityFactor * 255);
+            m_bSelected = _bSelected;
 			
-            if (iPenAlpha > 0)
+			using(Pen p = m_StyleHelper.GetPen(alpha, _transformer.Scale))
             {
-                m_fStretchFactor = _fStretchFactor;
-                m_DirectZoomTopLeft = new Point(_DirectZoomTopLeft.X, _DirectZoomTopLeft.Y);
-                RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
-
-                Pen penLine = m_StyleHelper.GetPen(iPenAlpha, m_fStretchFactor);
-                _canvas.DrawEllipse(penLine, m_RescaledOrigin.X - m_iRescaledRadius, m_RescaledOrigin.Y - m_iRescaledRadius, m_iRescaledDiameter, m_iRescaledDiameter);
+                Rectangle boundingBox = _transformer.Transform(m_Center.Box(m_iRadius));
+                _canvas.DrawEllipse(p, boundingBox);
                 
                 if(_bSelected)
                 {
-                	// Draw a small arc in complementary color in the lower right part to show resizer.
-                	penLine.Color = Color.FromArgb(iPenAlpha, 255 - m_StyleHelper.Color.R, 255 - m_StyleHelper.Color.G, 255 - m_StyleHelper.Color.B);
-					_canvas.DrawArc(penLine, m_RescaledOrigin.X - m_iRescaledRadius, m_RescaledOrigin.Y - m_iRescaledRadius, m_iRescaledDiameter, m_iRescaledDiameter, 25, 40);
+                	// Handler: arc in lower right quadrant.
+                	p.Color = p.Color.Invert();
+    				_canvas.DrawArc(p, boundingBox, 25, 40);
                 }
-                
-                penLine.Dispose();
             }
         }
         public override void MoveHandle(Point point, int handleNumber)
         {
-            // _point is mouse coordinates already descaled.
             // User is dragging the outline of the circle, figure out the new radius at this point.
-            int shiftX = Math.Abs(point.X - m_Origin.X);
-            int shiftY = Math.Abs(point.Y - m_Origin.Y);
+            int shiftX = Math.Abs(point.X - m_Center.X);
+            int shiftY = Math.Abs(point.Y - m_Center.Y);
             m_iRadius = (int)Math.Sqrt((shiftX*shiftX) + (shiftY*shiftY));
-            if(m_iRadius < 10) m_iRadius = 10;
-            m_iDiameter = m_iRadius * 2;
-            
-            // Update scaled coordinates accordingly.
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
+            if(m_iRadius < 10) 
+                m_iRadius = 10;
         }
         public override void MoveDrawing(int _deltaX, int _deltaY, Keys _ModifierKeys)
         {
-            // _delatX and _delatY are mouse delta already descaled.
-            m_Origin.X += _deltaX;
-            m_Origin.Y += _deltaY;
-
-            // Update scaled coordinates accordingly.
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
+            m_Center.X += _deltaX;
+            m_Center.Y += _deltaY;
         }
         public override int HitTest(Point _point, long _iCurrentTimestamp)
         {
-            //-----------------------------------------------------
-            // This function is used by the PointerTool 
-            // to know if we hit this particular drawing and where.
-            // _point is mouse coordinates already descaled.
-            // Hit Result: -1: miss, 0: on object, 1+: on handle.
-            //-----------------------------------------------------
+            // Convention: miss = -1, object = 0, handle = n.
             int iHitResult = -1;
             double fOpacityFactor = m_InfosFading.GetOpacityFactor(_iCurrentTimestamp);
             if (fOpacityFactor > 0)
             {
                 if (m_bSelected && IsPointOnHandler(_point))
-                {
                     iHitResult = 1;
-                }
                 else if (IsPointInObject(_point))
-				{
                     iHitResult = 0;
-                }
             }
             return iHitResult;
         }        
@@ -191,7 +158,7 @@ namespace Kinovea.ScreenManager
 				{
 					case "Origin":
 				        Point p = XmlHelper.ParsePoint(_xmlReader.ReadElementContentAsString());
-                        m_Origin = new Point((int)((float)p.X * _scale.X), (int)((float)p.Y * _scale.Y));
+                        m_Center = new Point((int)((float)p.X * _scale.X), (int)((float)p.Y * _scale.Y));
 				        break;
 					case "Radius":
 				        int radius = _xmlReader.ReadElementContentAsInt();
@@ -212,13 +179,10 @@ namespace Kinovea.ScreenManager
 			}
 			
 			_xmlReader.ReadEndElement();
-            
-			m_iDiameter = m_iRadius * 2;
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
         }
         public void WriteXml(XmlWriter _xmlWriter)
 		{
-            _xmlWriter.WriteElementString("Origin", String.Format("{0};{1}", m_Origin.X, m_Origin.Y));
+            _xmlWriter.WriteElementString("Origin", String.Format("{0};{1}", m_Center.X, m_Center.Y));
             _xmlWriter.WriteElementString("Radius", m_iRadius.ToString());
             
             _xmlWriter.WriteStartElement("DrawingStyle");
@@ -234,7 +198,7 @@ namespace Kinovea.ScreenManager
         #region IInitializable implementation
         public void ContinueSetup(Point point)
 		{
-			MoveHandle(point, 2);
+			MoveHandle(point, 1);
 		}
         #endregion
         
@@ -244,8 +208,7 @@ namespace Kinovea.ScreenManager
         }
         public override int GetHashCode()
         {
-            // Combine all relevant fields with XOR to get the Hash.
-            int iHash = m_Origin.GetHashCode();
+            int iHash = m_Center.GetHashCode();
             iHash ^= m_iRadius.GetHashCode();
             iHash ^= m_StyleHelper.GetHashCode();
             return iHash;
@@ -257,41 +220,25 @@ namespace Kinovea.ScreenManager
             m_Style.Bind(m_StyleHelper, "Color", "color");
 			m_Style.Bind(m_StyleHelper, "LineSize", "pen size");
         }
-        private void RescaleCoordinates(double _fStretchFactor, Point _DirectZoomTopLeft)
-        {
-        	m_RescaledOrigin = new Point((int)((double)(m_Origin.X - _DirectZoomTopLeft.X) * _fStretchFactor), (int)((double)(m_Origin.Y - _DirectZoomTopLeft.Y) * _fStretchFactor));
-        	m_iRescaledRadius = (int)((double)m_iRadius * _fStretchFactor);
-            m_iRescaledDiameter = m_iRescaledRadius * 2;
-        }
         private bool IsPointInObject(Point _point)
         {
-            // _point is already descaled.
-            // Note: AddEllipse adds a filled ellipse. The points inside the ellipse are hits.
-
             bool bIsPointInObject = false;
 			GraphicsPath areaPath = new GraphicsPath();
-			int wideRadius = m_iRadius + 10;
-			int wideDiameter = m_iDiameter + 20;
-			areaPath.AddEllipse(m_Origin.X - wideRadius, m_Origin.Y - wideRadius, wideDiameter, wideDiameter);
-			
-			// Create region from the path
+			areaPath.AddEllipse(m_Center.Box(m_iRadius + 10));
             bIsPointInObject = new Region(areaPath).IsVisible(_point);
-
             return bIsPointInObject;
         }
         private bool IsPointOnHandler(Point _point)
         {
-        	// _point is already descaled.
-            bool bIsPointOnHandler = false;
+        	bool bIsPointOnHandler = false;
             if(m_iRadius > 0)
             {
                 GraphicsPath areaPath = new GraphicsPath();			
-    			areaPath.AddArc(m_Origin.X - m_iRadius, m_Origin.Y - m_iRadius, m_iDiameter, m_iDiameter, 25, 40);
+                areaPath.AddArc(m_Center.Box(m_iRadius + 5), 25, 40);
     			
     			Pen areaPen = new Pen(Color.Black, m_StyleHelper.LineSize + 10);
     			areaPath.Widen(areaPen);
     			areaPen.Dispose();
-
                 bIsPointOnHandler = new Region(areaPath).IsVisible(_point);
             }
 			
--- ScreenManager/PlayerScreen/Drawings/DrawingCross2D.cs
+++ ScreenManager/PlayerScreen/Drawings/DrawingCross2D.cs
@@ -1,3 +1,4 @@
+#region License
 /*
 Copyright © Joan Charmant 2008.
 joan.charmant@gmail.com 
@@ -17,6 +18,7 @@ You should have received a copy of the GNU General Public License
 along with Kinovea. If not, see http://www.gnu.org/licenses/.
 
 */
+#endregion
 
 using System;
 using System.Collections.Generic;
@@ -79,9 +81,9 @@ namespace Kinovea.ScreenManager
         }
         
         // Next 2 props are accessed from Track creation.
-        public Point CenterPoint 
+        public Point Center 
 		{
-			get { return m_CenterPoint; }
+			get { return m_Center; }
 		}
         public Color PenColor
         {
@@ -90,37 +92,29 @@ namespace Kinovea.ScreenManager
         #endregion
 
         #region Members
-		// Position
-        private Point m_CenterPoint;           
-		private double m_fStretchFactor;
-        private Point m_DirectZoomTopLeft;
-        
-        // Decoration
+		// Core
+        private Point m_Center;           
+		private KeyframeLabel m_LabelCoordinates;
+		private bool m_bShowCoordinates;
+		// Decoration
         private StyleHelper m_StyleHelper = new StyleHelper();
         private DrawingStyle m_Style;
         private InfosFading m_InfosFading;
-		private KeyframeLabel m_LabelCoordinates;
-        private bool m_bShowCoordinates;
-        private Metadata m_ParentMetadata;
-        private static readonly int m_iDefaultBackgroundAlpha = 64;
-        private static readonly int m_iDefaultRadius = 3;
-
-        // Computed
-        private Point RescaledCenterPoint;
-        
+		
         // Context menu
         private ToolStripMenuItem mnuShowCoordinates = new ToolStripMenuItem();
         
+        private Metadata m_ParentMetadata;
+        private const int m_iDefaultBackgroundAlpha = 64;
+        private const int m_iDefaultRadius = 3;
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         #endregion
 
         #region Constructors
-        public DrawingCross2D(int x, int y, long _iTimestamp, long _iAverageTimeStampsPerFrame, DrawingStyle _preset)
+        public DrawingCross2D(Point _center, long _iTimestamp, long _iAverageTimeStampsPerFrame, DrawingStyle _preset)
         {
-            // Position
-            m_CenterPoint = new Point(x, y);
-            m_fStretchFactor = 1.0;
-            m_DirectZoomTopLeft = new Point(0, 0);
+            m_Center = _center;
+            m_LabelCoordinates = new KeyframeLabel(m_Center, Color.Black);
             
             // Decoration & binding with editors
             m_StyleHelper.Color = Color.CornflowerBlue;
@@ -132,17 +126,12 @@ namespace Kinovea.ScreenManager
                         
             m_InfosFading = new InfosFading(_iTimestamp, _iAverageTimeStampsPerFrame);
             
-            m_LabelCoordinates = new KeyframeLabel(m_CenterPoint, Color.Black);
-            
-            // Computed
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
-            
             // Context menu
             mnuShowCoordinates.Click += new EventHandler(mnuShowCoordinates_Click);
 			mnuShowCoordinates.Image = Properties.Drawings.measure;
         }
         public DrawingCross2D(XmlReader _xmlReader, PointF _scale, Metadata _parent)
-            : this(0,0,0,0, ToolManager.CrossMark.StylePreset.Clone())
+            : this(Point.Empty,0,0, ToolManager.CrossMark.StylePreset.Clone())
         {
             ReadXml(_xmlReader, _scale);
             m_ParentMetadata = _parent;
@@ -153,70 +142,48 @@ namespace Kinovea.ScreenManager
         public override void Draw(Graphics _canvas, CoordinateSystem _transformer, double _fStretchFactor, bool _bSelected, long _iCurrentTimestamp, Point _DirectZoomTopLeft)
         {
             double fOpacityFactor = m_InfosFading.GetOpacityFactor(_iCurrentTimestamp);
-            int iAlpha = (int)((double)255 * fOpacityFactor);
+            if(fOpacityFactor <= 0)
+                return;
+            
+            int iAlpha = (int)(fOpacityFactor * 255);
+            Point c = _transformer.Transform(m_Center);
 
-            if (iAlpha > 0)
+            using(Pen p = m_StyleHelper.GetPen(iAlpha))
+            using(SolidBrush b = m_StyleHelper.GetBrush((int)(fOpacityFactor * m_iDefaultBackgroundAlpha)))
             {
-                // Rescale the points.
-                m_fStretchFactor = _fStretchFactor;
-                m_DirectZoomTopLeft = new Point(_DirectZoomTopLeft.X, _DirectZoomTopLeft.Y);
-                RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
-
-                // Cross
-                Pen PenEdges = m_StyleHelper.GetPen(iAlpha);
-                _canvas.DrawLine(PenEdges, RescaledCenterPoint.X - m_iDefaultRadius, RescaledCenterPoint.Y, RescaledCenterPoint.X + m_iDefaultRadius, RescaledCenterPoint.Y);
-                _canvas.DrawLine(PenEdges, RescaledCenterPoint.X, RescaledCenterPoint.Y - m_iDefaultRadius, RescaledCenterPoint.X, RescaledCenterPoint.Y + m_iDefaultRadius);
-
-                // Background
-                SolidBrush tempBrush = m_StyleHelper.GetBrush((int)((double)m_iDefaultBackgroundAlpha * fOpacityFactor));
-                
-                _canvas.FillEllipse(tempBrush, RescaledCenterPoint.X - m_iDefaultRadius - 1, RescaledCenterPoint.Y - m_iDefaultRadius - 1, (m_iDefaultRadius * 2) + 2, (m_iDefaultRadius * 2) + 2);
-                tempBrush.Dispose();
-                PenEdges.Dispose();
-                
-                if(m_bShowCoordinates)
-                {
-                	m_LabelCoordinates.Text = m_ParentMetadata.CalibrationHelper.GetPointText(m_CenterPoint, true);
-	                m_LabelCoordinates.Draw(_canvas, _fStretchFactor, _DirectZoomTopLeft, fOpacityFactor);
-                }
+                _canvas.DrawLine(p, c.X - m_iDefaultRadius, c.Y, c.X + m_iDefaultRadius, c.Y);
+                _canvas.DrawLine(p, c.X, c.Y - m_iDefaultRadius, c.X, c.Y + m_iDefaultRadius);
+                _canvas.FillEllipse(b, c.Box(m_iDefaultRadius + 1));
+            }
+            
+            if(m_bShowCoordinates)
+            {
+            	m_LabelCoordinates.Text = m_ParentMetadata.CalibrationHelper.GetPointText(m_Center, true);
+                m_LabelCoordinates.Draw(_canvas, _transformer.Scale, _transformer.Location, fOpacityFactor);
             }
         }
         public override void MoveHandle(Point point, int handleNumber)
         {
-            // This is only implemented for the coordinates mini label.
             if(handleNumber == 1)
-            {
-		        m_LabelCoordinates.MoveLabel(point);
-            }
+                m_LabelCoordinates.MoveLabel(point);
         }
         public override void MoveDrawing(int _deltaX, int _deltaY, Keys _ModifierKeys)
         {
-            // _delatX and _delatY are mouse delta already descaled.
-            m_CenterPoint.X += _deltaX;
-            m_CenterPoint.Y += _deltaY;
-            
-            m_LabelCoordinates.MoveTo(m_CenterPoint);
-
-            // Update scaled coordinates accordingly.
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
+            m_Center.X += _deltaX;
+            m_Center.Y += _deltaY;
+            m_LabelCoordinates.MoveTo(m_Center);
         }
         public override int HitTest(Point _point, long _iCurrentTimestamp)
         {
-            // _point is mouse coordinates already descaled.
-            // Hit Result: -1: miss, 0: on object.
-            
+            // Convention: miss = -1, object = 0, handle = n.
             int iHitResult = -1;
             double fOpacityFactor = m_InfosFading.GetOpacityFactor(_iCurrentTimestamp);
             if (fOpacityFactor > 0)
             {
             	if(m_bShowCoordinates && m_LabelCoordinates.HitTest(_point))
-            	{
             		iHitResult = 1;
-            	}
-            	else if (IsPointInObject(_point))
-                {
+            	else if (m_Center.Box(m_iDefaultRadius + 10).Contains(_point))
                     iHitResult = 0;
-                }
             }
             
             return iHitResult;
@@ -234,7 +201,7 @@ namespace Kinovea.ScreenManager
 				{
 					case "CenterPoint":
 				        Point p = XmlHelper.ParsePoint(_xmlReader.ReadElementContentAsString());
-                        m_CenterPoint = new Point((int)((float)p.X * _scale.X), (int)((float)p.Y * _scale.Y));
+                        m_Center = new Point((int)((float)p.X * _scale.X), (int)((float)p.Y * _scale.Y));
 				        break;
 					case "CoordinatesVisible":
 				        m_bShowCoordinates = XmlHelper.ParseBoolean(_xmlReader.ReadElementContentAsString());
@@ -254,13 +221,11 @@ namespace Kinovea.ScreenManager
 			}
 			
 			_xmlReader.ReadEndElement();
-            
-			m_LabelCoordinates.MoveTo(CenterPoint);
-            RescaleCoordinates(m_fStretchFactor, m_DirectZoomTopLeft);
+			m_LabelCoordinates.MoveTo(Center);
         }
 		public void WriteXml(XmlWriter _xmlWriter)
 		{
-		    _xmlWriter.WriteElementString("CenterPoint", String.Format("{0};{1}", m_CenterPoint.X, m_CenterPoint.Y));
+		    _xmlWriter.WriteElementString("CenterPoint", String.Format("{0};{1}", m_Center.X, m_Center.Y));
             _xmlWriter.WriteElementString("CoordinatesVisible", m_bShowCoordinates ? "true" : "false");
             
             _xmlWriter.WriteStartElement("DrawingStyle");
@@ -276,7 +241,7 @@ namespace Kinovea.ScreenManager
             	// Spreadsheet support.
             	_xmlWriter.WriteStartElement("Coordinates");
             	
-            	PointF coords = m_ParentMetadata.CalibrationHelper.GetPointInUserUnit(m_CenterPoint);
+            	PointF coords = m_ParentMetadata.CalibrationHelper.GetPointInUserUnit(m_Center);
 	            _xmlWriter.WriteAttributeString("UserX", String.Format("{0:0.00}", coords.X));
 	            _xmlWriter.WriteAttributeString("UserXInvariant", String.Format(CultureInfo.InvariantCulture, "{0:0.00}", coords.X));
 	            _xmlWriter.WriteAttributeString("UserY", String.Format("{0:0.00}", coords.Y));
@@ -290,13 +255,11 @@ namespace Kinovea.ScreenManager
         
         public override string ToString()
         {
-            // Return the name of the tool used to draw this drawing.
             return ScreenManagerLang.ToolTip_DrawingToolCross2D;
         }
         public override int GetHashCode()
         {
-            // Combine all relevant fields with XOR to get the Hash.
-            int iHash = m_CenterPoint.GetHashCode();
+            int iHash = m_Center.GetHashCode();
             iHash ^= m_StyleHelper.GetHashCode();
             return iHash;
         }
@@ -318,23 +281,6 @@ namespace Kinovea.ScreenManager
         private void BindStyle()
         {
             m_Style.Bind(m_StyleHelper, "Color", "back color");
-        }
-        private void RescaleCoordinates(double _fStretchFactor, Point _DirectZoomTopLeft)
-        {
-            RescaledCenterPoint = new Point((int)((double)(m_CenterPoint.X - _DirectZoomTopLeft.X) * _fStretchFactor), (int)((double)(m_CenterPoint.Y - _DirectZoomTopLeft.Y) * _fStretchFactor));
-        }
-        private bool IsPointInObject(Point _point)
-        {
-            // Create path which contains wide line for easy mouse selection
-            GraphicsPath areaPath = new GraphicsPath();
-            Pen areaPen = new Pen(Color.Black, 7);
-
-            areaPath.AddLine(m_CenterPoint.X - m_iDefaultRadius, m_CenterPoint.Y, m_CenterPoint.X + m_iDefaultRadius, m_CenterPoint.Y);
-            areaPath.Widen(areaPen);
-            Region areaRegion = new Region(areaPath);
-            areaPen.Dispose();
-            
-            return areaRegion.IsVisible(_point);
         }
         #endregion
 
