c5261d9f1266424cbbe37f4b74f5de755fc2f2af
joan <joan@2c3a7681-93f5-4a91-9fda-68cc05e6b8ad>
2011-08-18 19:27:24 +0000

    [FileTypes] - Adding files that were missed in previous commit.

---
+++ Video/GIFVideoReader.cs
@@ -0,0 +1,61 @@
+﻿#region License
+/*
+Copyright © Joan Charmant 2011.
+joan.charmant@gmail.com 
+ 
+This file is part of Kinovea.
+
+Kinovea is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 
+as published by the Free Software Foundation.
+
+Kinovea is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Kinovea. If not, see http://www.gnu.org/licenses/.
+*/
+#endregion
+using System;
+
+namespace Kinovea.Video
+{
+    [SupportedExtensions(new string[] {".gif"})]
+    public class GIFVideoReader : VideoReader
+    {
+        public override bool Loaded
+        {
+            get { return false; }
+        }
+        public override VideoInfo Info
+        {
+            get { return m_VideoInfo; }
+        }
+        public override VideoSection Selection
+        {
+            get { return m_Selection; }
+        }
+        public override bool Caching
+        {
+            get { return true; }
+        }
+        public override VideoFrame Current
+        {
+            get { return null; }
+        }
+        public override OpenVideoResult Open(string _FilePath)
+        {
+            return OpenVideoResult.NotSupported;
+        }
+        public override void Close()
+        {
+            // TODO
+        }
+        
+        private VideoInfo m_VideoInfo;
+        private VideoSection m_Selection;
+        
+    }
+}
---
+++ Video/VideoInfo.cs
@@ -0,0 +1,79 @@
+﻿#region License
+/*
+Copyright © Joan Charmant 2011.
+joan.charmant@gmail.com 
+ 
+This file is part of Kinovea.
+
+Kinovea is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 
+as published by the Free Software Foundation.
+
+Kinovea is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Kinovea. If not, see http://www.gnu.org/licenses/.
+*/
+#endregion
+using System;
+using System.Drawing;
+
+namespace Kinovea.Video
+{
+ 
+    // TODO: hide those items that are implementation details.
+    
+    public struct VideoInfo
+    {
+        // Structural info
+        public string FilePath;
+        public Size DecodingSize;
+        
+        
+        
+        // Timing info
+        public long AverageTimeStampsPerFrame;
+        public double AverageTimeStampsPerSeconds;
+        public double FramesPerSeconds;
+        public double FrameIntervalMilliseconds;
+        
+        public long FirstTimeStamp;
+        public long LastTimeStamp;
+        public long DurationTimeStamps;
+        
+        /*public long	FileSize;
+        
+        public Size OutputSize;
+           
+        public double DecodingStretchFactor;			// Used to set the output size of image.
+        
+        public Fraction SampleAspectRatio;      
+        public double PixelAspectRatio;
+        
+                              // Should be a fraction ?
+        public bool FpsIsReliable;					
+        
+        
+        				// The first frame timestamp. (not always 0)
+        
+        
+        
+        */
+        
+        
+        
+        
+        
+        // Old fields:
+        //public ImageAspectRatio ImageAspectRatio;
+        //public int SampleAspectRatioNumerator;
+        //public int SampleAspectRatioDenominator;
+        //bool	bIsCodecMpeg2;					// Used to adapt pixel ratio on output.
+        
+        //int		iDecodingFlag;					// Quality of scaling during format conversion.
+        //bool	bDeinterlaced;					// If frames should be deinterlaced, this is the setting as set by the user.
+    }
+}
