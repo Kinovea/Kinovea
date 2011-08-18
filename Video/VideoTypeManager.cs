320b2788cd44a9a68c89c030f5683b3e406bc4a1
joan <joan@2c3a7681-93f5-4a91-9fda-68cc05e6b8ad>
2011-08-18 19:27:01 +0000

    [FileTypes] - Implementation of the basis of the plugin system for video readers. - Find all types deriving from VideoReader base class, either from the current assembly or assemblies found in special directory. - Register extensions supported by each VideoReader in a dictionary mapping the extension to the type. - When requested a reader from extension, lookup the dictionary and instanciate the right class.

---
+++ Video/VideoTypeManager.cs
@@ -0,0 +1,121 @@
+﻿#region License
+/*
+Copyright © Joan Charmant 2011.
+joan.charmant@gmail.com 
+ 
+This file is part of Kinovea.
+
+Kinovea is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 
+as published by the Free Software Foundation.
+
+Kinovea is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Kinovea. If not, see http://www.gnu.org/licenses/.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Kinovea.Video
+{
+    /// <summary>
+    /// Load and provide access to the list of video readers/writers.
+    /// </summary>
+    public static class VideoTypeManager
+    {
+        private static Dictionary<string, Type> m_VideoReaders = new Dictionary<string, Type>();
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        
+        /// <summary>
+        /// Find and register types implementing the VideoReader base class.
+        /// </summary>
+        public static void LoadVideoReaders()
+        {
+            // Check in this assembly and assemblies in the plugin folder.
+            List<Assembly> assemblies = new List<Assembly>();
+            assemblies.Add(typeof(VideoTypeManager).Assembly);
+            
+            string dir = Path.GetDirectoryName(Application.ExecutablePath) + "\\filetypes\\";
+            if(Directory.Exists(dir))
+            {
+                foreach (string fileName in Directory.GetFiles(dir, "*.dll"))
+                {
+                    Assembly pluginAssembly = null;
+                    try
+                    {
+                        pluginAssembly = Assembly.LoadFrom(fileName);
+                        assemblies.Add(pluginAssembly);
+                    }
+                    catch (Exception)
+                    {
+                        log.ErrorFormat("Could not load assembly {0} for file types plugin", fileName);
+                    }
+                }
+            }
+            
+            // Register the VideoReaders implementations with the extensions they support.
+            foreach (Assembly a in assemblies)
+            {
+                foreach(Type t in a.GetTypes())
+                {
+                    if(t.IsSubclassOf(typeof(VideoReader)) && !t.IsAbstract)
+                    {
+                        // Retrieve the extension list from the attribute, add each entry to the readers dictionary.
+                        object[] attributes = t.GetCustomAttributes(typeof(SupportedExtensionsAttribute), false);
+                        if(attributes.Length > 0)
+                        {
+                           string[] extensions = ((SupportedExtensionsAttribute)attributes[0]).Extensions;
+                           RegisterExtensions(extensions, t);
+                        }
+                    }
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Instanciate a video reader that supports the target extension.
+        /// </summary>
+        /// <param name="_extension"></param>
+        public static void GetVideoReader(string _extension)
+        {
+            // The FFMpeg plugin will support the wildcard extension as a fallback mechanism.
+            VideoReader reader = null;
+            Type readerType;
+            bool found = m_VideoReaders.TryGetValue(_extension, readerType);
+            
+            if(!found)
+                found = m_VideoReaders.TryGetValue(".*", readerType);
+            
+            if(found)
+            {
+                ConstructorInfo ci = t.GetConstructor(System.Type.EmptyTypes);
+                if(ci != null)
+                    reader = (VideoReader)Activator.CreateInstance(readerType, null);
+            }
+            
+            return reader;
+        }
+        
+        
+        #region Private Methods
+        private static void RegisterExtensions(string[] _extensions, Type _readerType)
+        {
+            log.DebugFormat("Registering extensions for {0} : {1}", _readerType.Name, string.Join("; ", _extensions));
+            
+            foreach(string extension in _extensions)
+            {
+                if(!m_VideoReaders.ContainsKey(extension))
+                    m_VideoReaders.Add(extension, _readerType);
+            }
+        }
+        #endregion
+    }
+}
