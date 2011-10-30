eefbf3d09645c9037cd655d4f0970f3f28fcc9d0
joan <joan@2c3a7681-93f5-4a91-9fda-68cc05e6b8ad>
2011-10-30 17:35:27 +0000

    [FileTypes] - Introduction of unit tests. - Usage of the NUnit framework. - Tests will be separated from the concrete project they tests. - Each project will have a mirror project for tests. - A new solution file was created, that reference all the regular projects plus tests projects. The regular solution file only has the code. - A first test project was added, hosting the namespace Kinovea.Video.Tests to group unit tests of Kinovea.Video classes. - Added test class for VideoSection (about 40 test cases).

---
+++ VideoTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿#region Using directives
+
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+[assembly: AssemblyTitle("Video.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Video.Tests")]
+[assembly: AssemblyCopyright("Copyright 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.*")]
---
+++ VideoTests/VideoSectionTests.cs
@@ -0,0 +1,114 @@
+﻿#region License
+/*
+Copyright © Joan Charmant 2011.
+joan.charmant@gmail.com 
+ 
+This file is part of Kinovea.
+
+Kinovea is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 
+as published by the Free Software Foundation.
+
+Kinovea is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Kinovea. If not, see http://www.gnu.org/licenses/.
+*/
+#endregion
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Kinovea.Video;
+
+namespace Kinovea.Video.Tests
+{
+    [TestFixture]
+    public class VideoSectionTests
+    {
+        [TestCase(-1, -1, Result=true)]
+        [TestCase(5, -1, Result=true)]
+        [TestCase(-1, 5, Result=true)]
+        [TestCase(0, 0, Result=false)]
+        [TestCase(5, 15, Result=false)]
+        [TestCase(15, 5, Result=false)]
+        public bool EmptynessTest(long _start, long _end)
+        {
+            VideoSection section = new VideoSection(_start, _end);
+            return section.IsEmpty;
+        }
+        
+        [TestCase(-1, -1, Result=false)]
+        [TestCase(5, -1, Result=false)]
+        [TestCase(-1, 5, Result=false)]
+        [TestCase(0, 0, Result=false)]
+        [TestCase(5, 15, Result=false)]
+        [TestCase(15, 5, Result=true)]
+        public bool WrappedTest(long _start, long _end)
+        {
+            VideoSection section = new VideoSection(_start, _end);
+            return section.Wrapped;
+        }
+        
+        [TestCase(-1, -1, 5, Result=false)]
+        [TestCase(5, -1, 3, Result=false)]
+        [TestCase(-1, 5, 3, Result=false)]
+        [TestCase(0, 0, 0, Result=true)]
+        [TestCase(0, 0, 5, Result=false)]
+        [TestCase(5, 15, 10, Result=true)]
+        [TestCase(5, 15, 20, Result=false)]
+        [TestCase(15, 5, 10, Result=false)]
+        [TestCase(15, 5, 20, Result=false)]
+        public bool ContainsValueTest(long _start, long _end, long _value)
+        {
+            VideoSection section = new VideoSection(_start, _end);
+            return section.Contains(_value);
+        }
+        
+        [TestCase(-1, -1, -1, -1, Result=false)]
+        [TestCase(5, 15, -1, -1, Result=false)]
+        [TestCase(5, 15, 0, 10, Result=false)]
+        [TestCase(5, 15, 5, 10, Result=true)]
+        [TestCase(5, 15, 8, 12, Result=true)]
+        [TestCase(5, 15, 10, 15, Result=true)]
+        [TestCase(5, 15, 10, 20, Result=false)]
+        [TestCase(5, 15, 20, 25, Result=false)]
+        public bool ContainsSectionTest(long _start, long _end, long _start2, long _end2)
+        {
+            VideoSection section = new VideoSection(_start, _end);
+            VideoSection section2 = new VideoSection(_start2, _end2);
+            return section.Contains(section2);
+        }
+        
+        [TestCase(-1, -1, -1, -1, Result=false)]
+        [TestCase(5, 15, -1, -1, Result=false)]
+        [TestCase(5, 15, 0, 10, Result=false)]
+        [TestCase(5, 15, 5, 10, Result=false)]
+        [TestCase(5, 15, 8, 12, Result=false)]
+        [TestCase(5, 15, 10, 15, Result=false)]
+        [TestCase(5, 15, 10, 25, Result=false)]
+        [TestCase(5, 15, 20, 25, Result=false)]
+        [TestCase(5, 15, 0, 15, Result=true)]
+        [TestCase(5, 15, 0, 20, Result=true)]
+        [TestCase(5, 15, 5, 20, Result=true)]
+        public bool OperatorLessThanTest(long _start, long _end, long _start2, long _end2)
+        {
+            VideoSection section = new VideoSection(_start, _end);
+            VideoSection section2 = new VideoSection(_start2, _end2);
+            return section < section2;
+        }
+        
+        [TestCase(-1, -1, -1, -1, Result=false)]
+        [TestCase(5, 15, -1, -1, Result=false)]
+        [TestCase(5, 15, 5, 15, Result=true)]
+        [TestCase(5, 15, 5, 10, Result=false)]
+        public bool OperatorEqualsTest(long _start, long _end, long _start2, long _end2)
+        {
+            VideoSection section = new VideoSection(_start, _end);
+            VideoSection section2 = new VideoSection(_start2, _end2);
+            return section == section2;
+        }
+    }
+}
