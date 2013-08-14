815c328f2529a0d98ef79c3c2045f5817c37cd6d
Joan Charmant <joan.charmant@gmail.com>
2013-08-14 23:06:45 +0200

    Cosmetics :?untabify.

--- FileBrowser/Commands/CommandDeleteShortcut.cs
+++ FileBrowser/Commands/CommandDeleteShortcut.cs
@@ -32,9 +32,9 @@ namespace Kinovea.FileBrowser
 {
     public class CommandDeleteShortcut : IUndoableCommand
     {
-		public string FriendlyName
+        public string FriendlyName
         {
-        	get { return FileBrowserLang.mnuDeleteShortcut; }
+            get { return FileBrowserLang.mnuDeleteShortcut; }
         }
 
         #region Members
@@ -45,8 +45,8 @@ namespace Kinovea.FileBrowser
         #region Constructor
         public CommandDeleteShortcut(FileBrowserUserInterface _FbUi, ShortcutFolder _shortcut)
         {
-        	m_FbUi = _FbUi;
-        	m_shortcut = _shortcut;
+            m_FbUi = _FbUi;
+            m_shortcut = _shortcut;
         }
         #endregion
 
@@ -54,17 +54,17 @@ namespace Kinovea.FileBrowser
         {
             PreferencesManager.FileExplorerPreferences.RemoveShortcut(m_shortcut);
             PreferencesManager.Save();
-        	
-        	// Refresh the list.
-			m_FbUi.ReloadShortcuts();
+            
+            // Refresh the list.
+            m_FbUi.ReloadShortcuts();
         }
 
         public void Unexecute()
         {
-        	// Add the shortcut back to the list (if it hasn't been added again in the meantime).
-        	PreferencesManager.FileExplorerPreferences.AddShortcut(m_shortcut);
-        	PreferencesManager.Save();
-        	m_FbUi.ReloadShortcuts();
+            // Add the shortcut back to the list (if it hasn't been added again in the meantime).
+            PreferencesManager.FileExplorerPreferences.AddShortcut(m_shortcut);
+            PreferencesManager.Save();
+            m_FbUi.ReloadShortcuts();
         }
     }
 }
