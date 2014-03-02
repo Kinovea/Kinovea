bdae1d3eb88e5b6c646b3445b30c5e14d3be25ec
Joan Charmant <joan.charmant@gmail.com>
2014-03-02 10:17:10 +0100

    Revert the way the total duration of the clip is computed: do not include the last frame interval in the duration.

--- Kinovea.ScreenManager/PlayerScreen/TimeType.cs
+++ Kinovea.ScreenManager/PlayerScreen/TimeType.cs
@@ -5,15 +5,9 @@ using System.Text;
 
 namespace Kinovea.ScreenManager
 {
-    /// <summary>
-    /// Time is used to denote a single time snapshot and will be converted to a corresponding instant in time.
-    /// Duration is for stopwatch and other type of time.
-    /// TotalDuration denotes the total number of time unit, so it's duration + 1 frame. Used for the duration of the video.
-    /// </summary>
     public enum TimeType
     {
         Time,
-        Duration,
-        TotalDuration 
+        Duration
     }
 }
