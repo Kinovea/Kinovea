2e2a3bd51d6ab231a0cbacf0c3e27f41e13fe3ad
Joan Charmant <jcharmant@gmail.com>
2014-06-19 20:00:15 +0200

    [Rafale export] - Simplification of the feature and dialog. The dialog now asks for a decimation value and works from there to compute how many frames to export and the frame interval.

--- Kinovea.ScreenManager/PlayerScreen/UserInterface/FormRafaleExport.Designer.cs
+++ Kinovea.ScreenManager/PlayerScreen/UserInterface/FormRafaleExport.Designer.cs
@@ -1,6 +1,6 @@
-namespace Kinovea.ScreenManager
+ï»¿namespace Kinovea.ScreenManager
 {
-    partial class formRafaleExport
+    partial class FormRafaleExport
     {
         /// <summary>
         /// Required designer variable.
@@ -28,31 +28,80 @@ namespace Kinovea.ScreenManager
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpboxConfig = new System.Windows.Forms.GroupBox();
+            this.lblTimeDecimation = new System.Windows.Forms.Label();
+            this.lblFrameDecimation = new System.Windows.Forms.Label();
+            this.trkDecimate = new System.Windows.Forms.TrackBar();
+            this.chkKeyframesOnly = new System.Windows.Forms.CheckBox();
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
-            this.grpboxInfos = new System.Windows.Forms.GroupBox();
-            this.lblInfosFileSuffix = new System.Windows.Forms.Label();
-            this.lblInfosTotalSeconds = new System.Windows.Forms.Label();
-            this.lblInfosTotalFrames = new System.Windows.Forms.Label();
-            this.lblInfosFrequency = new System.Windows.Forms.Label();
-            this.grpboxConfig = new System.Windows.Forms.GroupBox();
-            this.chkKeyframesOnly = new System.Windows.Forms.CheckBox();
-            this.freqViewer = new Kinovea.ScreenManager.FrequencyViewer();
-            this.chkBlend = new System.Windows.Forms.CheckBox();
-            this.trkInterval = new System.Windows.Forms.TrackBar();
-            this.grpboxInfos.SuspendLayout();
+            this.lblTotalFrames = new System.Windows.Forms.Label();
             this.grpboxConfig.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trkInterval)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkDecimate)).BeginInit();
             this.SuspendLayout();
+            // 
+            // grpboxConfig
+            // 
+            this.grpboxConfig.BackColor = System.Drawing.Color.White;
+            this.grpboxConfig.Controls.Add(this.lblTotalFrames);
+            this.grpboxConfig.Controls.Add(this.lblTimeDecimation);
+            this.grpboxConfig.Controls.Add(this.lblFrameDecimation);
+            this.grpboxConfig.Controls.Add(this.trkDecimate);
+            this.grpboxConfig.Location = new System.Drawing.Point(7, 46);
+            this.grpboxConfig.Name = "grpboxConfig";
+            this.grpboxConfig.Size = new System.Drawing.Size(280, 152);
+            this.grpboxConfig.TabIndex = 32;
+            this.grpboxConfig.TabStop = false;
+            this.grpboxConfig.Text = "Configuration";
+            // 
+            // lblTimeDecimation
+            // 
+            this.lblTimeDecimation.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTimeDecimation.AutoSize = true;
+            this.lblTimeDecimation.Location = new System.Drawing.Point(20, 96);
+            this.lblTimeDecimation.Name = "lblTimeDecimation";
+            this.lblTimeDecimation.Size = new System.Drawing.Size(135, 13);
+            this.lblTimeDecimation.TabIndex = 16;
+            this.lblTimeDecimation.Text = "Export one image every 2s.";
+            // 
+            // lblFrameDecimation
+            // 
+            this.lblFrameDecimation.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblFrameDecimation.AutoSize = true;
+            this.lblFrameDecimation.Location = new System.Drawing.Point(20, 74);
+            this.lblFrameDecimation.Name = "lblFrameDecimation";
+            this.lblFrameDecimation.Size = new System.Drawing.Size(166, 13);
+            this.lblFrameDecimation.TabIndex = 1;
+            this.lblFrameDecimation.Text = "Export one image every 5 images.";
+            // 
+            // trkDecimate
+            // 
+            this.trkDecimate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.trkDecimate.Location = new System.Drawing.Point(6, 24);
+            this.trkDecimate.Name = "trkDecimate";
+            this.trkDecimate.Size = new System.Drawing.Size(268, 45);
+            this.trkDecimate.TabIndex = 5;
+            this.trkDecimate.ValueChanged += new System.EventHandler(this.trkDecimate_ValueChanged);
+            // 
+            // chkKeyframesOnly
+            // 
+            this.chkKeyframesOnly.AutoSize = true;
+            this.chkKeyframesOnly.Location = new System.Drawing.Point(12, 12);
+            this.chkKeyframesOnly.Name = "chkKeyframesOnly";
+            this.chkKeyframesOnly.Size = new System.Drawing.Size(137, 17);
+            this.chkKeyframesOnly.TabIndex = 15;
+            this.chkKeyframesOnly.Text = "Export key images only.";
+            this.chkKeyframesOnly.UseVisualStyleBackColor = true;
+            this.chkKeyframesOnly.CheckedChanged += new System.EventHandler(this.chkKeyframesOnly_CheckedChanged);
             // 
             // btnOK
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOK.Location = new System.Drawing.Point(262, 359);
+            this.btnOK.Location = new System.Drawing.Point(91, 211);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(99, 24);
-            this.btnOK.TabIndex = 20;
-            this.btnOK.Text = "Enregistrer";
+            this.btnOK.TabIndex = 29;
+            this.btnOK.Text = "Save";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
@@ -60,166 +109,58 @@ namespace Kinovea.ScreenManager
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.btnCancel.Location = new System.Drawing.Point(367, 359);
+            this.btnCancel.Location = new System.Drawing.Point(196, 211);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(99, 24);
-            this.btnCancel.TabIndex = 25;
-            this.btnCancel.Text = "Annuler";
+            this.btnCancel.TabIndex = 30;
+            this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             // 
-            // grpboxInfos
+            // lblTotalFrames
             // 
-            this.grpboxInfos.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.grpboxInfos.Controls.Add(this.lblInfosFileSuffix);
-            this.grpboxInfos.Controls.Add(this.lblInfosTotalSeconds);
-            this.grpboxInfos.Controls.Add(this.lblInfosTotalFrames);
-            this.grpboxInfos.Location = new System.Drawing.Point(13, 233);
-            this.grpboxInfos.Name = "grpboxInfos";
-            this.grpboxInfos.Size = new System.Drawing.Size(454, 120);
-            this.grpboxInfos.TabIndex = 27;
-            this.grpboxInfos.TabStop = false;
-            this.grpboxInfos.Text = "Informations";
+            this.lblTotalFrames.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTotalFrames.AutoSize = true;
+            this.lblTotalFrames.Location = new System.Drawing.Point(20, 121);
+            this.lblTotalFrames.Name = "lblTotalFrames";
+            this.lblTotalFrames.Size = new System.Drawing.Size(99, 13);
+            this.lblTotalFrames.TabIndex = 17;
+            this.lblTotalFrames.Text = "Total exported : 83.";
             // 
-            // lblInfosFileSuffix
-            // 
-            this.lblInfosFileSuffix.AutoSize = true;
-            this.lblInfosFileSuffix.Location = new System.Drawing.Point(14, 82);
-            this.lblInfosFileSuffix.Name = "lblInfosFileSuffix";
-            this.lblInfosFileSuffix.Size = new System.Drawing.Size(352, 13);
-            this.lblInfosFileSuffix.TabIndex = 7;
-            this.lblInfosFileSuffix.Text = "Le suffixe sera ajouté automatiquement aux images. ( => fichier.00.00.jpg)";
-            // 
-            // lblInfosTotalSeconds
-            // 
-            this.lblInfosTotalSeconds.AutoSize = true;
-            this.lblInfosTotalSeconds.Location = new System.Drawing.Point(12, 54);
-            this.lblInfosTotalSeconds.Name = "lblInfosTotalSeconds";
-            this.lblInfosTotalSeconds.Size = new System.Drawing.Size(326, 13);
-            this.lblInfosTotalSeconds.TabIndex = 2;
-            this.lblInfosTotalSeconds.Text = "Durée couverte par la séquence d\'images exportées : 20 secondes.";
-            // 
-            // lblInfosTotalFrames
-            // 
-            this.lblInfosTotalFrames.AutoSize = true;
-            this.lblInfosTotalFrames.Location = new System.Drawing.Point(11, 27);
-            this.lblInfosTotalFrames.Name = "lblInfosTotalFrames";
-            this.lblInfosTotalFrames.Size = new System.Drawing.Size(220, 13);
-            this.lblInfosTotalFrames.TabIndex = 2;
-            this.lblInfosTotalFrames.Text = "Nombre total d\'images exportées : 83 images.";
-            // 
-            // lblInfosFrequency
-            // 
-            this.lblInfosFrequency.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblInfosFrequency.AutoSize = true;
-            this.lblInfosFrequency.Location = new System.Drawing.Point(11, 71);
-            this.lblInfosFrequency.Name = "lblInfosFrequency";
-            this.lblInfosFrequency.Size = new System.Drawing.Size(363, 13);
-            this.lblInfosFrequency.TabIndex = 1;
-            this.lblInfosFrequency.Text = "Fréquence d\'enregistrement : une image tous les 40 centièmes de seconde.";
-            // 
-            // grpboxConfig
-            // 
-            this.grpboxConfig.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.grpboxConfig.BackColor = System.Drawing.Color.White;
-            this.grpboxConfig.Controls.Add(this.chkKeyframesOnly);
-            this.grpboxConfig.Controls.Add(this.freqViewer);
-            this.grpboxConfig.Controls.Add(this.lblInfosFrequency);
-            this.grpboxConfig.Controls.Add(this.chkBlend);
-            this.grpboxConfig.Controls.Add(this.trkInterval);
-            this.grpboxConfig.Location = new System.Drawing.Point(12, 12);
-            this.grpboxConfig.Name = "grpboxConfig";
-            this.grpboxConfig.Size = new System.Drawing.Size(454, 211);
-            this.grpboxConfig.TabIndex = 28;
-            this.grpboxConfig.TabStop = false;
-            this.grpboxConfig.Text = "Configuration";
-            // 
-            // chkKeyframesOnly
-            // 
-            this.chkKeyframesOnly.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.chkKeyframesOnly.AutoSize = true;
-            this.chkKeyframesOnly.Location = new System.Drawing.Point(12, 175);
-            this.chkKeyframesOnly.Name = "chkKeyframesOnly";
-            this.chkKeyframesOnly.Size = new System.Drawing.Size(213, 17);
-            this.chkKeyframesOnly.TabIndex = 15;
-            this.chkKeyframesOnly.Text = "Enregistrer uniquement les Images Clés.";
-            this.chkKeyframesOnly.UseVisualStyleBackColor = true;
-            this.chkKeyframesOnly.CheckedChanged += new System.EventHandler(this.chkKeyframesOnly_CheckedChanged);
-            // 
-            // freqViewer
-            // 
-            this.freqViewer.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.freqViewer.HorizontalLines = 2;
-            this.freqViewer.Interval = 1000;
-            this.freqViewer.Location = new System.Drawing.Point(15, 28);
-            this.freqViewer.Name = "freqViewer";
-            this.freqViewer.Size = new System.Drawing.Size(419, 27);
-            this.freqViewer.TabIndex = 8;
-            this.freqViewer.Total = 10000;
-            // 
-            // chkBlend
-            // 
-            this.chkBlend.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.chkBlend.AutoSize = true;
-            this.chkBlend.Checked = true;
-            this.chkBlend.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.chkBlend.Location = new System.Drawing.Point(12, 143);
-            this.chkBlend.Name = "chkBlend";
-            this.chkBlend.Size = new System.Drawing.Size(193, 17);
-            this.chkBlend.TabIndex = 10;
-            this.chkBlend.Text = "Incruster les dessins sur les images.";
-            this.chkBlend.UseVisualStyleBackColor = true;
-            // 
-            // trkInterval
-            // 
-            this.trkInterval.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.trkInterval.Location = new System.Drawing.Point(13, 92);
-            this.trkInterval.Name = "trkInterval";
-            this.trkInterval.Size = new System.Drawing.Size(422, 45);
-            this.trkInterval.TabIndex = 5;
-            this.trkInterval.ValueChanged += new System.EventHandler(this.trkInterval_ValueChanged);
-            // 
-            // formRafaleExport
+            // FormRafaleExport2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(479, 395);
+            this.ClientSize = new System.Drawing.Size(303, 242);
             this.Controls.Add(this.grpboxConfig);
-            this.Controls.Add(this.grpboxInfos);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.chkKeyframesOnly);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.Name = "formRafaleExport";
+            this.Name = "FormRafaleExport2";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "   Enregistrer une Séquence d\'Images";
-            this.grpboxInfos.ResumeLayout(false);
-            this.grpboxInfos.PerformLayout();
+            this.Text = "Rafale export";
             this.grpboxConfig.ResumeLayout(false);
             this.grpboxConfig.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.trkInterval)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trkDecimate)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox grpboxConfig;
+        private System.Windows.Forms.Label lblTimeDecimation;
+        private System.Windows.Forms.CheckBox chkKeyframesOnly;
+        private System.Windows.Forms.Label lblFrameDecimation;
+        private System.Windows.Forms.TrackBar trkDecimate;
         private System.Windows.Forms.Button btnOK;
         private System.Windows.Forms.Button btnCancel;
-        private System.Windows.Forms.GroupBox grpboxInfos;
-        private System.Windows.Forms.Label lblInfosTotalSeconds;
-        private System.Windows.Forms.Label lblInfosFrequency;
-        private System.Windows.Forms.GroupBox grpboxConfig;
-        private System.Windows.Forms.Label lblInfosTotalFrames;
-        private System.Windows.Forms.CheckBox chkBlend;
-        private System.Windows.Forms.TrackBar trkInterval;
-        private FrequencyViewer freqViewer;
-        private System.Windows.Forms.Label lblInfosFileSuffix;
-        private System.Windows.Forms.CheckBox chkKeyframesOnly;
+        private System.Windows.Forms.Label lblTotalFrames;
     }
 }
