00eff9b073b024b5e5d3e984bc51f0c7e660fd1e
Joan Charmant <joan.charmant@gmail.com>
2014-11-23 14:10:05 +0100

    [Capture] - Added MJPEGWriter and ConsumerMJPEGRecorder. Enabled toggle record back.

---
+++ Kinovea.ScreenManager/CaptureScreen/ConsumerMJPEGRecorder.cs
@@ -0,0 +1,53 @@
+﻿using Kinovea.Pipeline;
+using Kinovea.Pipeline.Consumers;
+using System.IO;
+using System;
+using Kinovea.Video;
+using Kinovea.Video.FFMpeg;
+using System.Drawing;
+
+namespace Kinovea.ScreenManager
+{
+    /// <summary>
+    /// ConsumerMJPEGRecorder. Save samples to an MJPEG file (in MP4 container).
+    /// The recorder is format agnostic, the format is simply passed along to the writer.
+    /// The writer will decide if resampling and encoding are needed.
+    /// </summary>
+    public class ConsumerMJPEGRecorder : AbstractConsumer
+    {
+        private string filename;
+        private ImageDescriptor imageDescriptor;
+        private MJPEGWriter writer;
+        
+        public SaveResult Prepare(string filename, double interval, ImageDescriptor imageDescriptor)
+        {
+            this.imageDescriptor = imageDescriptor;
+
+            if (writer != null)
+                writer.Dispose();
+
+            writer = new MJPEGWriter();
+            
+            VideoInfo info = new VideoInfo();
+            info.OriginalSize = new Size(imageDescriptor.Width, imageDescriptor.Height);
+
+            SaveResult result = writer.OpenSavingContext(filename, info, interval);
+
+            return result;
+        }
+
+        protected override void AfterDeactivate()
+        {
+            writer.CloseSavingContext(true);
+            writer.Dispose();
+            writer = null;
+
+            base.AfterDeactivate();
+        }
+
+        protected override void ProcessEntry(long position, Frame entry)
+        {
+            writer.SaveFrame(imageDescriptor.Format, entry.Buffer, entry.PayloadLength);
+        }
+    }
+}
