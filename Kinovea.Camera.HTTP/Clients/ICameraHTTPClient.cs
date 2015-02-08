c0772842ea4ff214cacfacfd3b32ed0239dee279
Joan Charmant <jcharmant@gmail.com>
2015-02-08 18:52:29 +0100

    [Capture] - Restoration of HTTP camera module functionality.

---
+++ Kinovea.Camera.HTTP/Clients/ICameraHTTPClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Video;
+
+namespace Kinovea.Camera.HTTP
+{
+    public interface ICameraHTTPClient
+    {
+        // At the moment these events are still dependent on AForge for convenience.
+        // Ideally these would be redeclared locally and each CameraHTTPClient would be a proxy 
+        // from events declared in the underlying library.
+        event NewFrameBufferEventHandler NewFrameBuffer;
+        event NewFrameEventHandler NewFrame;
+        event VideoSourceErrorEventHandler VideoSourceError;
+
+        bool IsRunning { get; set; }
+
+        void Start();
+        void Stop();
+        void SignalToStop();
+    }
+}
