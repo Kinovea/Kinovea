ac3d3576daecf9db248018dbd9ab7bf9b5217545
Joan Charmant <jcharmant@gmail.com>
2016-08-14 12:43:27 +0200

    Update copyright to 2016.

--- Kinovea.Video/Properties/AssemblyInfo.cs
+++ Kinovea.Video/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Kinovea")]
 [assembly: AssemblyProduct("Kinovea")]
-[assembly: AssemblyCopyright("Copyright © 2011 - Joan Charmant and contributors.")]
+[assembly: AssemblyCopyright("Copyright © 2006-2016 - Joan Charmant and contributors.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("1.0.0")]
