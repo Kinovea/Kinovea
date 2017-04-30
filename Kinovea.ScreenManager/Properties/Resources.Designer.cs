4203346200da322f08fd347324456ba406b185f0
Joan Charmant <jcharmant@gmail.com>
2017-04-30 09:50:10 +0200

    Removed unused resource.

--- Kinovea.ScreenManager/Properties/Resources.Designer.cs
+++ Kinovea.ScreenManager/Properties/Resources.Designer.cs
@@ -153,16 +153,6 @@ namespace Kinovea.ScreenManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap capturedock4 {
-            get {
-                object obj = ResourceManager.GetObject("capturedock4", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap capturedock5 {
             get {
                 object obj = ResourceManager.GetObject("capturedock5", resourceCulture);
