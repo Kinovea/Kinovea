1ad5a7dddd48cc466ee666db0f14fba790aafe95
Joan Charmant <jcharmant@gmail.com>
2018-09-12 21:27:31 +0200

    Change style element "curved" into "toggle" and add the concept of toggle variants to support perspective option on grid/plane.

--- Kinovea.ScreenManager/Metadata/Drawings/Styling/StyleElementToggle.cs
+++ Kinovea.ScreenManager/Metadata/Drawings/Styling/StyleElementToggle.cs
@@ -31,11 +31,9 @@ namespace Kinovea.ScreenManager
 {
     /// <summary>
     /// Style element to represent a boolean value used by the drawing.
-    /// At the moment this is specific to the "curved" value for polylines, 
-    /// but this could probably be factored when another boolean value arise.
     /// Editor: checkbox.
     /// </summary>
-    public class StyleElementCurved : AbstractStyleElement
+    public class StyleElementToggle : AbstractStyleElement
     {
         #region Properties
         public override object Value
@@ -49,15 +47,15 @@ namespace Kinovea.ScreenManager
         }
         public override Bitmap Icon
         {
-            get { return Properties.Drawings.curve; }
+            get { return icon; }
         }
         public override string DisplayName
         {
-            get { return ScreenManagerLang.Generic_Curved; }
+            get { return displayName; }
         }
         public override string XmlName
         {
-            get { return "Curved"; }
+            get { return "Toggle"; }
         }
         #endregion
 
@@ -65,15 +63,18 @@ namespace Kinovea.ScreenManager
 
         #region Members
         private bool value;
+        private Bitmap icon;
+        private StyleToggleVariant variant = StyleToggleVariant.Unknown;
+        private string displayName;
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         #endregion
 
         #region Constructor
-        public StyleElementCurved(bool initialValue)
+        public StyleElementToggle(bool initialValue)
         {
             value = initialValue;
         }
-        public StyleElementCurved(XmlReader xmlReader)
+        public StyleElementToggle(XmlReader xmlReader)
         {
             ReadXML(xmlReader);
         }
@@ -90,20 +91,50 @@ namespace Kinovea.ScreenManager
         }
         public override AbstractStyleElement Clone()
         {
-            AbstractStyleElement clone = new StyleElementCurved(value);
+            StyleElementToggle clone = new StyleElementToggle(value);
+            clone.icon = icon;
+            clone.displayName = displayName;
+            clone.variant = variant;
             clone.Bind(this);
             return clone;
         }
         public override void ReadXML(XmlReader xmlReader)
         {
-            xmlReader.ReadStartElement();
-            string s = xmlReader.ReadElementContentAsString("Value", "");
-            value = XmlHelper.ParseBoolean(s);
+            xmlReader.ReadStartElement();	// <Toggle>
+
+            while (xmlReader.NodeType == XmlNodeType.Element)
+            {
+                switch (xmlReader.Name)
+                {
+                    case "Value":
+                        value = XmlHelper.ParseBoolean(xmlReader.ReadElementContentAsString());
+                        break;
+                    case "Variant":
+                        variant = (StyleToggleVariant)Enum.Parse(typeof(StyleToggleVariant), xmlReader.ReadElementContentAsString());
+                        break;
+                    default:
+                        string unparsed = xmlReader.ReadOuterXml();
+                        log.DebugFormat("Unparsed content in style element toggle XML: {0}", unparsed);
+                        break;
+                }
+            }
+
             xmlReader.ReadEndElement();
+
+            // If the variant is unknown, the most likely cause is that it's an older file where "Curved" was assumed.
+            // Worst case scenario from doing this is that we show "Curved" instead of "Unknown" in the mini editor.
+            if (variant == StyleToggleVariant.Unknown)
+            {
+                variant = StyleToggleVariant.Curved;
+                log.ErrorFormat("Unknown variant in style element toggle. Assumed: {0}", variant.ToString());
+            }
+    
+            UpdateVariant(variant);
         }
         public override void WriteXml(XmlWriter xmlWriter)
         {
             xmlWriter.WriteElementString("Value", value.ToString().ToLower());
+            xmlWriter.WriteElementString("Variant", variant.ToString());
         }
         #endregion
 
@@ -112,6 +143,22 @@ namespace Kinovea.ScreenManager
         {
             value = ((CheckBox)sender).Checked;
             RaiseValueChanged();
+        }
+        private void UpdateVariant(StyleToggleVariant variant)
+        {
+            // Update the appearance of the mini editor based on the variant.
+            switch (variant)
+            {
+                case StyleToggleVariant.Perspective:
+                    icon = Properties.Drawings.plane;
+                    displayName = ScreenManagerLang.mnuPerspectiveGrid;
+                    break;
+                case StyleToggleVariant.Curved:
+                default:
+                    icon = Properties.Drawings.curve;
+                    displayName = ScreenManagerLang.Generic_Curved;
+                    break;
+            }
         }
         #endregion
     }
