c62629e6c5ba1412b5d63342ddb1715c51ecdaee
Joan Charmant <jcharmant@gmail.com>
2018-09-13 11:30:05 +0200

    Improve cursors for tools (precision cursors and stamp cursors).

--- Kinovea.ScreenManager/CursorManager.cs
+++ Kinovea.ScreenManager/CursorManager.cs
@@ -14,8 +14,25 @@ namespace Kinovea.ScreenManager
             if (tool is DrawingToolPointer)
                 return ((DrawingToolPointer)tool).GetCursor();
 
-            if (tool is DrawingTool)
+            // Custom cursors.
+            // The general guideline is that 
+            // - We try to use custom cursor where it make sense like for pencil.
+            // - Otherwise we fallback to either precision cross or the tool icon.
+            // - Drawings that splat themselves at once on the canvas (no dragging a second leg), should use the icon,
+            // - Drawings that are created in several steps should use the precision cross.
+
+            if (tool is DrawingToolGenericPosture || tool is DrawingToolAutoNumbers)
             {
+                // Many of the GenericPosture tools have a color style element that doesn't really serve any purpose, 
+                // as the lines making the drawing can have their own color defined in the XML.
+                // They are also draw at once on the canvas like a stamp.
+                // So for these we reuse the drawing tool icon as a cursor.
+                return GetCursorIcon(tool);
+            }
+            else if (tool is DrawingTool)
+            {
+                // These are the standard tool but defined in XML.
+                // For these we try to use a precision cursor or a semantic one.
                 DrawingStyle style = ToolManager.GetStylePreset(tool.Name);
 
                 if (tool.Name == "Pencil")
@@ -26,18 +43,28 @@ namespace Kinovea.ScreenManager
                 {
                     return GetCursorCrossMark(style);
                 }
+                else if (tool.Name == "Grid" || 
+                         tool.Name == "Plane" || 
+                         tool.Name == "DistortionGrid" ||
+                         tool.Name == "Label")
+                {
+                    // These are stamp-like.
+                    return GetCursorIcon(tool);
+                }
                 else
                 {
-                    return Cursors.Cross;
+                    return GetCursorPrecision(style);
                 }
             }
             else if (tool is DrawingToolCoordinateSystem ||
-                     tool is DrawingToolAutoNumbers ||
-                     tool is DrawingToolSpotlight ||
-                     tool is DrawingToolGenericPosture ||
+                     tool is DrawingToolMagnifier||
+                     tool is DrawingToolSpotlight || 
                      tool is DrawingToolTestGrid)
             {
-                return Cursors.Cross;
+                // Special internal tools. 
+                // Still nice to use a precision cursor with them, but the color might not make sense.
+                DrawingStyle style = ToolManager.GetStylePreset(tool.Name);
+                return GetCursorPrecision(style);
             }
             else
             {
@@ -45,9 +72,21 @@ namespace Kinovea.ScreenManager
             }
         }
 
+        private static Cursor GetCursorIcon(AbstractDrawingTool tool)
+        {
+            if (tool.Icon == null)
+                return Cursors.Cross;
+
+            Bitmap b = new Bitmap(tool.Icon.Width, tool.Icon.Height);
+            Graphics g = Graphics.FromImage(b);
+            g.DrawImage(tool.Icon, 0, 0);
+
+            return new Cursor(b.GetHicon());
+        }
+
         private static Cursor GetCursorPencil(DrawingStyle style, double stretchFactor)
         {
-            // Custom cursor: Colored and sized circle.
+            // Colored and sized circle.
             string keyColor = "color";
             string keySize = "pen size";
 
@@ -64,6 +103,8 @@ namespace Kinovea.ScreenManager
             Bitmap b = new Bitmap(size + 2, size + 2);
             Graphics g = Graphics.FromImage(b);
             g.DrawEllipse(p, 1, 1, size, size);
+
+            g.Dispose();
             p.Dispose();
 
             return new Cursor(b.GetHicon());
@@ -71,7 +112,7 @@ namespace Kinovea.ScreenManager
 
         private static Cursor GetCursorCrossMark(DrawingStyle style)
         {
-            // Custom cursor: cross inside a semi transparent circle (same as drawing).
+            // Cross inside a semi transparent circle (same as drawing).
             string keyColor = "back color";
             if (!style.Elements.ContainsKey(keyColor))
                 return null;
@@ -88,6 +129,46 @@ namespace Kinovea.ScreenManager
             SolidBrush tempBrush = new SolidBrush(Color.FromArgb(32, c));
             g.FillEllipse(tempBrush, 0, 0, 8, 8);
             tempBrush.Dispose();
+
+            g.Dispose();
+            p.Dispose();
+
+            return new Cursor(b.GetHicon());
+        }
+        
+        private static Cursor GetCursorPrecision(DrawingStyle style)
+        {
+            // Try to find a color style element to use as the cursor color.
+            Color color = Color.Empty;
+            if (style != null && style.Elements != null && style.Elements.Count > 0)
+            {
+                foreach (AbstractStyleElement styleElement in style.Elements.Values)
+                {
+                    if (styleElement is StyleElementColor)
+                    {
+                        object value = ((StyleElementColor)styleElement).Value;
+                        if (value is Color)
+                        {
+                            color = (Color)value;
+                        }
+                    }
+                }
+            }
+
+            if (color.IsEmpty)
+                color = Color.White;
+
+            // Creates a "precision" cursor, simple cross.
+            Pen p = new Pen(color, 1);
+            int size = 15;
+            int half = (size - 1) / 2;
+            Bitmap b = new Bitmap(size, size);
+            Graphics g = Graphics.FromImage(b);
+            
+            g.DrawLine(p, 0, half, size-1, half);
+            g.DrawLine(p, half, 0, half, size-1);
+
+            g.Dispose();
             p.Dispose();
 
             return new Cursor(b.GetHicon());
