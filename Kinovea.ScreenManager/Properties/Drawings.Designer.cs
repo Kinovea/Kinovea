b73d62676e6b1aa0d2d28b0ab96b6e2fec3c4ee8
Joan Charmant <joan.charmant@gmail.com>
2018-09-16 01:41:22 +0200

    Add icon for rectangle tool.

--- Kinovea.ScreenManager/Properties/Drawings.Designer.cs
+++ Kinovea.ScreenManager/Properties/Drawings.Designer.cs
@@ -643,6 +643,16 @@ namespace Kinovea.ScreenManager.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap rectangle {
+            get {
+                object obj = ResourceManager.GetObject("rectangle", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap spotlight {
             get {
                 object obj = ResourceManager.GetObject("spotlight", resourceCulture);
