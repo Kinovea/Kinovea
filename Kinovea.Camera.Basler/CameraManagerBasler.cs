a8ea8c86ef0b64fa96fa813bac488602e9394f72
Joan Charmant <joan.charmant@gmail.com>
2018-09-21 13:42:16 +0200

    Fix copy of the PylonC.NET.dll to the build output folder.

--- Kinovea.Camera.Basler/CameraManagerBasler.cs
+++ Kinovea.Camera.Basler/CameraManagerBasler.cs
@@ -37,6 +37,13 @@ namespace Kinovea.Camera.Basler
     /// </summary>
     public class CameraManagerBasler : CameraManager
     {
+        //---------------------------------------------------------------------------------------------------------
+        // Note: The .NET framework will only delay-load the assembly as we don't need it in any public field.
+        // This means the Pylon wrapper DLL will not be copied to the output directory.
+        // Even if we use it here, since the Pylon installer added it to the GAC, it won't be copied to the output.
+        // The copy is done explicitely in kinovea.targets.
+        //---------------------------------------------------------------------------------------------------------
+
         #region Properties
         public override bool Enabled
         {
@@ -76,6 +83,7 @@ namespace Kinovea.Camera.Basler
             bool result = false;
             try
             {
+                
                 Pylon.Initialize();
                 result = true;
             }
