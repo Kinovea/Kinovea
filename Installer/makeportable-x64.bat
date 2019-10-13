cd544124c440ea6652b578f2f10e8bd363d801cf
Joan Charmant <jcharmant@gmail.com>
2019-10-13 14:25:02 +0200

    Automate zipping of the portable version archive after building the installer.

--- Installer/makeportable-x64.bat
+++ Installer/makeportable-x64.bat
@@ -1,2 +1,9 @@
+REM Rebuild and generate the installer.
 makensis /DPORTABLE /DREBUILD /DX64 kinovea.nsi > build-portable-x64.txt
+
+REM Run the installer, it will extract itself locally.
 forfiles /p "." /m Kinovea-Portable-*-x64.exe /c "@file"
+
+REM Zip the content.
+cd Kinovea-0.9.1-x64
+7z a -r ../Kinovea-0.9.1-x64.zip *
