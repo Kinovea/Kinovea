440fc50194967aa42854522538da1276b5686d31
Joan Charmant <jcharmant@gmail.com>
2020-02-02 12:27:26 +0100

    Factorize file framerate adjustment function.

--- Kinovea.ScreenManager/CaptureScreen/ConsumerRealtime.cs
+++ Kinovea.ScreenManager/CaptureScreen/ConsumerRealtime.cs
@@ -70,26 +70,7 @@ namespace Kinovea.ScreenManager
 
             bool uncompressed = PreferencesManager.CapturePreferences.SaveUncompressedVideo && imageDescriptor.Format != ImageFormat.JPEG;
             string formatString = FilenameHelper.GetFormatStringCapture(uncompressed);
-
-            // If the capture happens too fast or too slow for a regular player, set the video metadata to a more sensible framerate.
-            // This avoids erratic playback because the player can't cope with the framerate, drawback: prevents review in real time.
-            double hrft = PreferencesManager.CapturePreferences.HighspeedRecordingFramerateThreshold;
-            double srft = PreferencesManager.CapturePreferences.SlowspeedRecordingFramerateThreshold;
-            double fps = 1000.0 / interval;
-            double fileInterval = interval;
-            if (fps >= hrft)
-            {
-                double hrfo = PreferencesManager.CapturePreferences.HighspeedRecordingFramerateOutput;
-                fileInterval = 1000.0 / hrfo;
-                log.DebugFormat("High speed recording detected, {0:0.###} fps. Forcing output framerate to {1:0.###} fps.", fps, hrfo);
-            }
-            else if (fps <= srft)
-            {
-                double srfo = PreferencesManager.CapturePreferences.SlowspeedRecordingFramerateOutput;
-                fileInterval = 1000.0 / srfo;
-                log.DebugFormat("Slow speed recording detected, {0:0.###} fps. Forcing output framerate to {1:0.###} fps.", fps, srfo);
-            }
-
+            double fileInterval = CalibrationHelper.FileFrameInterval(interval);
 
             log.DebugFormat("Frame budget for writer [{0}]: {1:0.000} ms.", shortId, interval);
 
--- Kinovea.ScreenManager/Measurement/Calibration/CalibrationHelper.cs
+++ Kinovea.ScreenManager/Measurement/Calibration/CalibrationHelper.cs
@@ -594,6 +594,33 @@ namespace Kinovea.ScreenManager
         }
         #endregion
 
+        #region Capture framerate
+        public static double FileFrameInterval(double interval)
+        {
+            // If the capture happens too fast or too slow for a regular player, set the video metadata to a more sensible framerate.
+            // This avoids erratic playback because the player can't cope with the framerate, drawback: prevents review in real time.
+            double hrft = PreferencesManager.CapturePreferences.HighspeedRecordingFramerateThreshold;
+            double srft = PreferencesManager.CapturePreferences.SlowspeedRecordingFramerateThreshold;
+            double fps = 1000.0 / interval;
+            double fileInterval = interval;
+            if (fps >= hrft)
+            {
+                double hrfo = PreferencesManager.CapturePreferences.HighspeedRecordingFramerateOutput;
+                fileInterval = 1000.0 / hrfo;
+                log.DebugFormat("High speed recording detected, {0:0.###} fps. Forcing output framerate to {1:0.###} fps.", fps, hrfo);
+            }
+            else if (fps <= srft)
+            {
+                double srfo = PreferencesManager.CapturePreferences.SlowspeedRecordingFramerateOutput;
+                fileInterval = 1000.0 / srfo;
+                log.DebugFormat("Slow speed recording detected, {0:0.###} fps. Forcing output framerate to {1:0.###} fps.", fps, srfo);
+            }
+
+            return fileInterval;
+        }
+        #endregion
+
+
         #region Private helpers
         private void AfterCalibrationChanged()
         {
