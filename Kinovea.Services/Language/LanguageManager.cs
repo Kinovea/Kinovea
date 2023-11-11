cf4b927946bf026228a6447bd6fff32b54227ead
Joan Charmant <jcharmant@gmail.com>
2023-11-11 20:41:48 +0100

    Cherry pick translation commits from master, up to aff536ce2e21917f5749348779e12e21e275c98f.
    Including: Japanese, Croatian, Finnish, Korean, German, Indonesian.

--- Kinovea.Services/Language/LanguageManager.cs
+++ Kinovea.Services/Language/LanguageManager.cs
@@ -1,12 +1,12 @@
 ﻿#region License
 /*
 Copyright © Joan Charmant 2011.
-jcharmant@gmail.com 
- 
+jcharmant@gmail.com
+
 This file is part of Kinovea.
 
 Kinovea is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 2 
+it under the terms of the GNU General Public License version 2
 as published by the Free Software Foundation.
 
 Kinovea is distributed in the hope that it will be useful,
@@ -30,15 +30,15 @@ namespace Kinovea.Services
     {
         public static Dictionary<string, string> Languages
         {
-            get 
-            { 
+            get
+            {
                 return languages;
             }
         }
-        
+
         private static Dictionary<string, string> languages = null;
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        
+
         static LanguageManager()
         {
             // Alphabetical order by native name. (Check Wikipedia order if in doubt).
@@ -57,6 +57,7 @@ namespace Kinovea.Services
             languages.Add("fa", "فارسی");
             languages.Add("fr", "Français");
             languages.Add("ko", "한국어");
+            languages.Add("hr", "Hrvatski");
             languages.Add("id", "Bahasa Indonesia");
             languages.Add("it", "Italiano");
             languages.Add("lt", "Lietuvių");
@@ -85,22 +86,22 @@ namespace Kinovea.Services
             // Rationale:
             // It is frustrating and look unprofessional for users to select their language only to realize
             // that many parts of the interface are not actually translated.
-            // 
+            //
             // Criteria:
             // 1. > 85% translated in total -> kept.
             // 2. < 50% translated in total -> removed.
             // 3. For the remaining languages, we look at
-            // - the Root component (top level menu and preferences), 
+            // - the Root component (top level menu and preferences),
             // - how the front UI looks like, File, View and Image, and the UI of the player.
             // If there are too many untranslated strings directly visible in the menu and the player UI
             // we do not include the language.
 
             // Reference: https://hosted.weblate.org/projects/kinovea/#languages
+            // Last check: 2023-10-02.
 
             // Languages with less than 50% translation coverage.
             languages.Remove("da"); // Danish.
             languages.Remove("el"); // Greek.
-            languages.Remove("fi"); // Finnish.
             languages.Remove("ms"); // Malay.
             languages.Remove("no"); // Norwegian.
             languages.Remove("sr-Latn-RS"); // Serbo-croatian.
@@ -108,7 +109,6 @@ namespace Kinovea.Services
             // Languages between 50% and 85%.
             //languages.Remove("bg"); // Bulgarian.
             //languages.Remove("fa"); // Farsi.
-            languages.Remove("ja"); // Japanese.
             languages.Remove("sr-Cyrl-RS"); // Serbian.
             languages.Remove("sv"); // Swedish.
             languages.Remove("th"); // Thai.
