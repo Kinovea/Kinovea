5dfa48699144d72976db8aaed76b6bc650d14e90
Joan Charmant <jcharmant@gmail.com>
2024-05-16 16:19:32 +0200

    Update IDS plugin to uEye 2.0.1.0 from IDS software suite 4.96.

--- Kinovea.Camera.IDS/Properties/AssemblyInfo.cs
+++ Kinovea.Camera.IDS/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
