f14bd744c00e6fd43a127370b6da11ffc08e5deb
Joan Charmant <jcharmant@gmail.com>
2024-10-02 15:47:12 +0200

    Update version number.

--- Kinovea/Properties/AssemblyInfo.cs
+++ Kinovea/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Resources;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("b049b83e-90f3-4e84-9289-52ee6ea2a9ea")]
-[assembly: AssemblyVersion("2024.1.0.0")]
-[assembly: AssemblyFileVersion("2024.1.0.0")]
+[assembly: AssemblyVersion("2024.2.0.0")]
+[assembly: AssemblyFileVersion("2024.2.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
 
 [assembly: log4net.Config.XmlConfigurator(ConfigFile = "LogConf.xml", Watch = true)]
