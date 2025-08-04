f5bfcf2780899cdcefce396fd3218637a5020e52
Joan Charmant <jcharmant@gmail.com>
2025-08-04 14:56:21 +0200

    Fix capture folder selector.
    Cherry picked from master: 9daef3bb95de50bf6c119afcc3dee17b8e638c7e
    (2024-11-15)

--- Kinovea/UserInterface/PreferencePanels/PreferencePanelCapture.cs
+++ Kinovea/UserInterface/PreferencePanels/PreferencePanelCapture.cs
@@ -482,7 +482,7 @@ namespace Kinovea.Root
                 initialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
 
             string path = FilesystemHelper.OpenFolderBrowserDialog(initialDirectory);
-            if (string.IsNullOrEmpty(path))
+            if (!string.IsNullOrEmpty(path))
                 tb.Text = path;
         }
 
