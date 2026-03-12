57c2cb1edd6927d756ffb215175464693feeb963
Joan Charmant <jcharmant@gmail.com>
2026-03-13 00:14:50 +0100

    Minor update to udp-client.py demo script.

--- Tools/remote/udp-client.py
+++ Tools/remote/udp-client.py
@@ -2,17 +2,16 @@ import os
 import socket
 import time
 
-# Every 10 seconds send an empty udp broadcast to 192.168.1.255 port 8875
+# Send an empty udp broadcast every 10 seconds.
 def broadcast():
     print(f'Broadcasting at {time.ctime()}')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    #s.sendto(b'', ('192.168.1.255', 8875))
-    s.sendto(b'', ('127.0.0.1', 8870))
+    s.sendto(b'', ('192.168.0.228', 8875))
     s.close()
 
 
 if __name__ == '__main__':
     while True:
         broadcast()
-        time.sleep(5)
+        time.sleep(10)
